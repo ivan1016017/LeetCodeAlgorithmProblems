9ac587eb5c7e539d4509f447dc8d21163c524f66
Ivan Penaloza <ivan1016017@gmail.com>
2021-08-16 08:31:24 -0500

    group_special_equivalent.py

---
+++ src/my_project/medium_problems/from51to100/group_special_equivalent.py
@@ -0,0 +1,15 @@
+from typing import List
+
+class Solution:
+    def numSpecialEquivGroups(self, words: List[str]) -> int:
+        hset=set()
+        for i in range(len(words)):
+            even=[0]*26
+            odd=[0]*26
+            for j in range(len(words[i])):
+                if j%2==0:
+                    even[ord(words[i][j])-ord('a')]+=1
+                else:
+                    odd[ord(words[i][j])-ord('a')]+=1
+            hset.add("".join([str(even[i]) for i in range(len(even))])+"".join([str(odd[i]) for i in range(len(odd))]))
+        return len(hset)
