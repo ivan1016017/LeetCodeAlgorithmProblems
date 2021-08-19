ec4d15e20bb1b9924e7dc849df6e84965f408274
Ivan Penaloza <ivan1016017@gmail.com>
2021-08-19 07:55:36 -0500

    more efficient solution to the interval_list_intersection.py

--- src/my_project/medium_problems/from51to100/interval_list_intersections.py
+++ src/my_project/medium_problems/from51to100/interval_list_intersections.py
@@ -1,7 +1,7 @@
 from typing import List
 
 
-class Solution:
+class SecondSolution:
     def intervalIntersection(self, firstList: List[List[int]], secondList: List[List[int]]) -> List[List[int]]:
 
         solution = list()
@@ -16,6 +16,31 @@ class Solution:
 
 
         return solution
+
+class Solution:
+    def intervalIntersection(self, firstList: List[List[int]], secondList: List[List[int]]) -> List[List[int]]:
+        i, j = 0, 0
+        l1, l2 = len(firstList), len(secondList)
+        res = []
+
+        while i<l1 and j<l2:
+            if secondList[j][0]>firstList[i][1]:
+                i+=1
+            elif secondList[j][1]<firstList[i][0]:
+                j+=1
+            elif secondList[j][0]>=firstList[i][0]:
+                res.append([max(secondList[j][0], firstList[i][0]), min(secondList[j][1], firstList[i][1])])
+                if j<l2-1 and secondList[j+1][0]<=firstList[i][1]:
+                    j+=1
+                else:
+                    i+=1
+            elif secondList[j][0]<=firstList[i][0]:
+                res.append([max(secondList[j][0], firstList[i][0]), min(secondList[j][1], firstList[i][1])])
+                if i<l1-1 and firstList[i+1][0]<=secondList[j][1]:
+                    i+=1
+                else:
+                    j+=1
+        return res
 
 
 solution = Solution()
