edd439930face1e357eeb0a44caab522134baf8e
Ivan Penaloza <ivan1016017@gmail.com>
2021-08-31 08:28:58 -0500

    monotonic_array.py

---
+++ src/my_project/easy_problems/from101to150/monotonc_array.py
@@ -0,0 +1,47 @@
+from typing import List
+
+class Solution:
+    def isMonotonic(self, nums: List[int]) -> bool:
+        len_nums = len(nums)
+
+        if len_nums == 1:
+            return True
+
+        flag = 0
+        solution = True
+
+        for i in range(1,len_nums):
+
+            if nums[i] - nums[i-1] > 0:
+                flag = 1
+                break
+            elif nums[i] - nums[i-1] < 0:
+                flag = -1
+                break
+
+        if flag == 0:
+            return solution
+
+
+        if flag == 1:
+            for i in range(1,len_nums):
+                if nums[i] - nums[i-1] < 0:
+                    return False
+            return solution
+
+        if flag == -1:
+            for i in range(1, len_nums):
+                if nums[i] - nums[i-1] > 0:
+                    return False
+            return solution
+
+solution = Solution()
+
+print(solution.isMonotonic(nums = [1,1,1]))
+
+
+
+
+
+
+
