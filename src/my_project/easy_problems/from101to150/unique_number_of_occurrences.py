7cc77ad5c53c2319696c09703904f1e21db1ea71
Ivan Penaloza <ivan1016017@gmail.com>
2021-09-04 07:39:33 -0500

    unique_number_of_occurrences.py

---
+++ src/my_project/easy_problems/from101to150/unique_number_of_occurrences.py
@@ -0,0 +1,28 @@
+from typing import List
+from collections import defaultdict, OrderedDict
+
+class Solution:
+    def uniqueOccurrences(self, arr: List[int]) -> bool:
+
+        order_dict = defaultdict(int)
+
+        for i in arr:
+            order_dict[i] += 1
+
+
+
+        solution: List[int] = list()
+        solution_set = set()
+
+        for i in order_dict.values():
+            solution.append(i)
+
+        solution_set = set(solution)
+
+        return len(solution) == len(solution_set)
+
+solution = Solution()
+
+print(solution.uniqueOccurrences(arr = [1,2,2,1,1,3]))
+
+print(solution.uniqueOccurrences(arr = [1,2]))
