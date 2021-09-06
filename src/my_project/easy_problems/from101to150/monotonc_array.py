b3624ee0cf35ae8fc4dba045779fdb0c5a0e151a
Ivan Penaloza <ivan1016017@gmail.com>
2021-09-06 07:28:29 -0500

    new changes to file monotonic_array.py

--- src/my_project/easy_problems/from101to150/monotonc_array.py
+++ src/my_project/easy_problems/from101to150/monotonc_array.py
@@ -2,6 +2,7 @@ from typing import List
 
 class Solution:
     def isMonotonic(self, nums: List[int]) -> bool:
+        # Inititalize variables
         len_nums = len(nums)
 
         if len_nums == 1:
@@ -22,7 +23,6 @@ class Solution:
         if flag == 0:
             return solution
 
-
         if flag == 1:
             for i in range(1,len_nums):
                 if nums[i] - nums[i-1] < 0:
@@ -36,7 +36,6 @@ class Solution:
             return solution
 
 solution = Solution()
-
 print(solution.isMonotonic(nums = [1,1,1]))
 
 
