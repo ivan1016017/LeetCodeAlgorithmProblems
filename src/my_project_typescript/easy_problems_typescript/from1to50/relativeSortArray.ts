73e8730c8854eaa28cb29a5efc76ced8c28f71d6
Ivan Penaloza <ivan1016017@gmail.com>
2021-09-18 17:45:38 -0500

    I deleted some code intentionally

--- src/my_project_typescript/easy_problems_typescript/from1to50/relativeSortArray.ts
+++ src/my_project_typescript/easy_problems_typescript/from1to50/relativeSortArray.ts
@@ -17,7 +17,9 @@ function relativeSortArray(arr1: number[], arr2: number[]): number[] {
             leftArray.push(item)
         }
     }
+// this is a new comment how to revert changes in git 
 
+let myVar: number = 0
     for (let item of arr1){
         if (!arr2.includes(item)){
             rightArray.push(item)
