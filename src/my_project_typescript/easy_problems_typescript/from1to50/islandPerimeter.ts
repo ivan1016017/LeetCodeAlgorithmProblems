80e790d2b4dd64f0a4e49067e8f8d7308137410f
Ivan Penaloza <ivan1016017@gmail.com>
2021-09-23 07:54:34 -0500

    islandPerimeter.ts

---
+++ src/my_project_typescript/easy_problems_typescript/from1to50/islandPerimeter.ts
@@ -0,0 +1,22 @@
+function islandPerimeter(grid: number[][]): number {
+
+    let perimeter: number = 0;
+
+    for (let i = 0; i < grid.length; i++){
+        for (let j = 0; j < grid[0].length; j++){
+            if (grid[i][j] ==1){
+                perimeter += 4
+                if (i > 0 && grid[i-1][j] == 1){
+                    perimeter -= 2
+                }
+                if (j > 0 && grid[i][j-1]){
+                    perimeter -= 2
+                }
+            }
+        }
+    }
+    return perimeter
+
+};
+
+console.log(islandPerimeter([[0,1,0,0],[1,1,1,0],[0,1,0,0],[1,1,0,0]]))
