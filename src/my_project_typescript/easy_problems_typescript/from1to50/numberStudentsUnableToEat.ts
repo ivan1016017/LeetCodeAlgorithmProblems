00460b1140224eac73ef30c7983f09237935ed0b
Ivan Penaloza <ivan1016017@gmail.com>
2021-09-24 08:12:24 -0500

    new function myFuntion to be reverted

--- src/my_project_typescript/easy_problems_typescript/from1to50/numberStudentsUnableToEat.ts
+++ src/my_project_typescript/easy_problems_typescript/from1to50/numberStudentsUnableToEat.ts
@@ -23,6 +23,12 @@ function countStudents(students: number[], sandwiches: number[]): number {
 
 };
 
+// this a new variable or function
+
+function myFunction(firstVariable: number) : number {
+    return firstVariable * 2
+}
+
 console.log(countStudents( [1,1,0,0], [0,1,0,1]))
 console.log(countStudents( [1,1,1,0,0,1], [1,0,0,0,1,1]))
 
