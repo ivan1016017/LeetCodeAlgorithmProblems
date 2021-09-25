77913fa15c3b87ed13e9fd6f83e7b9ea5cf2785e
Ivan Penaloza <ivan1016017@gmail.com>
2021-09-25 08:51:46 -0500

    no changes

--- src/my_project_typescript/easy_problems_typescript/from1to50/matrixCellsDistanceOrder.ts
+++ src/my_project_typescript/easy_problems_typescript/from1to50/matrixCellsDistanceOrder.ts
@@ -15,11 +15,7 @@ function allCellsDistOrder(rows: number, cols: number, rCenter: number, cCenter:
     
     return answer
 };
-// function to be deleted
 
-function myFunction(newVariable: number){
-    return newVariable
-}
 
 allCellsDistOrder( 2,  2,  0,  1)
 allCellsDistOrder( 2, 3, 1, 2)
