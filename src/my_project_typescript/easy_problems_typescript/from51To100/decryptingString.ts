af292e3d933a93fd5a9ebdbcb721071f7b13eca5
Ivan Penaloza <ivan1016017@gmail.com>
2021-10-07 13:44:17 -0500

    decrypt string

---
+++ src/my_project_typescript/easy_problems_typescript/from51To100/decryptingString.ts
@@ -0,0 +1,30 @@
+function freqAlphabets(s: string): string {
+    let answer: string = ""
+
+    let dict_characters:any = {
+        '1':'a','2':'b','3':'c','4':'d','5':'e','6':'f','7':'g','8':'h','9':'i',
+        '10#':'j', '11#':'k','12#':'l','13#':'m','14#':'n','15#':'o','16#':'p',
+        '17#':'q','18#':'r','19#':'s','20#':'t','21#':'u','22#':'v','23#':'w','24#':'x',
+        '25#':'y','26#':'z'
+    }
+    
+
+    let i = s.length -1 
+
+    while (i >= 0){
+        
+        
+        if (s[i] == "#"){
+            answer = dict_characters[s.slice(i-2, i+1)] + answer
+            i = i - 3
+        }
+        else {
+            
+            answer = dict_characters[s[i]] + answer
+            
+            i = i - 1
+        }
+    }
+
+    return answer
+};
