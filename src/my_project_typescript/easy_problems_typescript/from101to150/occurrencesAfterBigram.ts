5306ada9fe09ea6d802cd7aa7b5364f4a32a43cd
Ivan Penaloza <ivan1016017@gmail.com>
2021-11-08 07:54:08 -0600

    function to be reverted

--- src/my_project_typescript/easy_problems_typescript/from101to150/occurrencesAfterBigram.ts
+++ src/my_project_typescript/easy_problems_typescript/from101to150/occurrencesAfterBigram.ts
@@ -21,3 +21,9 @@ console.log(findOcurrences("we will we will rock you",  "we", "will"))
 
 console.log(findOcurrences("alice is a good girl she is a good",  "a",  "good"))
 
+
+// function to be reverted using commands in git 
+function toBeReverted(nums: number) : number{
+    return nums*7
+}
+
