72c2e19e1f87c202d326a212bf17976695b12131
Ivan Penaloza <ivan1016017@gmail.com>
2021-11-17 08:02:12 -0600

    string matching in array algorithm in python

---
+++ src/my_project/easy_problems/from201to250/string_matching_in_array.py
@@ -0,0 +1,21 @@
+from typing import List 
+
+class Solution:
+    def stringMatching(self, words: List[str]) -> List[str]:
+        
+        
+        res = set()
+        
+        for word1 in words:
+            for word2 in words:
+                if word1 != word2 and word1 in word2:
+                    res.add(word1)
+                    
+        return res 
+        
+ 
+    
+    
+solution = Solution()
+
+print(solution.stringMatching(words = ["mass","as","hero","superhero"]))
