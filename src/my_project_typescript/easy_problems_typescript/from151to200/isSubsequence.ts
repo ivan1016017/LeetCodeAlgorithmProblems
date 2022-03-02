1be69a1339d2cfee3bc59fbf99e215717bea4feb
Ivan Penaloza <ivan1016017@gmail.com>
2022-03-02 07:49:30 -0600

    is subsequence algorithm in typescript

---
+++ src/my_project_typescript/easy_problems_typescript/from151to200/isSubsequence.ts
@@ -0,0 +1,14 @@
+function isSubsequence(s: string, t: string): boolean {
+    let i:number = 0
+    let j: number = 0
+
+    while (i < s.length && j < t.length){
+        if (s[i] == t[j]){
+            i += 1
+        }
+        j += 1
+    }
+
+    return i === s.length
+
+};
