5e0f93c798a5553c0da7acfadf13150ec0a9d3e7
Ivan Penaloza <ivan1016017@gmail.com>
2022-03-21 07:52:48 -0600

    python

---
+++ src/my_project/easy_problems/from251to300/most_frequent_number_following_key.py
@@ -0,0 +1,19 @@
+from typing import List 
+import collections
+
+class Solution:
+    def mostFrequent(self, nums: List[int], key: int) -> int:
+        dic = collections.defaultdict(int)
+        ans = float("inf")
+        temp = 0
+        for i in range(len(nums)-1):
+            if nums[i] == key:
+                dic[nums[i+1]] += 1
+                if temp < dic[nums[i+1]]:
+                    ans = nums[i+1]
+                    temp = dic[nums[i+1]]
+        
+        return ans
+        
+        
+        return None 
