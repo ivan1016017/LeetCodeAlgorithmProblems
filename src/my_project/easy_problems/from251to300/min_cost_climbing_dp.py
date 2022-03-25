fb043a8f9244d15217416b9a3e633c5a60d0efb7
Ivan Penaloza <ivan1016017@gmail.com>
2022-03-25 07:36:42 -0600

    dp solution in python

---
+++ src/my_project/easy_problems/from251to300/min_cost_climbing_dp.py
@@ -0,0 +1,7 @@
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        l = len(cost)
+        dp = [0]*(l+2)
+        for i in range(3,l+2):
+            dp[i] = min(dp[i-1]+cost[i-2],dp[i-2]+cost[i-3])
+        return dp[l+1]
