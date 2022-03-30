c496dba5595a44b6e01843bb56729df852c83646
Ivan Penaloza <ivan1016017@gmail.com>
2022-03-30 08:06:24 -0600

    solution python

--- src/my_project/easy_problems/from251to300/find_difference_between_two_arrays.py
+++ src/my_project/easy_problems/from251to300/find_difference_between_two_arrays.py
@@ -3,6 +3,83 @@ from typing import List
 class Solution:
     def findDifference(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
         
+        list_one = list()
+        list_two = list()
+        
+        nums1 = list(set(nums1))
+        nums2 = list(set(nums2))
+        
+        
+        num_total = nums1 + nums2
+        
+        dict1 = dict()
+        dict2 = dict()
+        
+        
+        for num in num_total:
+            dict1[num] = 0
+            dict2[num] = 0
+            
+        for num in nums1:
+            dict1[num] += 1
+        
+        for num in nums2:
+            dict2[num] += 1
+            
+        for num in nums2:
+            if dict1[num] == 0:
+                list_one.append(num)
+                
+        for num in nums1:
+            if dict2[num] == 0:
+                list_two.append(num)
+                
+        return [list_two, list_one]
+    
+    
+    
+from typing import List 
+
+class Solution:
+    def findDifference(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
+        
+        list_one = list()
+        list_two = list()
+        
+        nums1 = list(set(nums1))
+        nums2 = list(set(nums2))
+        
+        
+        num_total = nums1 + nums2
+        
+        dict1 = dict()
+        dict2 = dict()
+        
+        
+        for num in num_total:
+            dict1[num] = 0
+            dict2[num] = 0
+            
+        for num in nums1:
+            dict1[num] += 1
+        
+        for num in nums2:
+            dict2[num] += 1
+            
+        for num in nums2:
+            if dict1[num] == 0:
+                list_one.append(num)
+                
+        for num in nums1:
+            if dict2[num] == 0:
+                list_two.append(num)
+                
+        return [list_two, list_one]
+    
+    
+class SolutionTwo:
+    def findDifference(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
+        
         l1=set()
         l2 = set()
         nums1 = set(nums1)
