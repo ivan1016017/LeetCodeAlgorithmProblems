5110e83c3339e5eec6b73cf5ef02338c1d772523
Ivan Penaloza <ivan1016017@gmail.com>
2022-04-03 09:53:02 -0500

    merge sorted

---
+++ src/my_project/easy_problems/from251to300/merge_sorted_array.py
@@ -0,0 +1,8 @@
+from typing import List 
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        
+        nums1[m:] = nums2[:n]
+        
+        nums1.sort()
