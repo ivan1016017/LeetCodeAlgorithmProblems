446fc61f480a527ff81f45146c3ecdda994d82ec
Ivan Penaloza <ivan1016017@gmail.com>
2022-04-11 07:57:36 -0500

    python solution

---
+++ src/my_project/easy_problems/from251to300/power_of_three.py
@@ -0,0 +1,22 @@
+from typing import List 
+
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        
+        if n < 1:
+            return False 
+        
+        elif n == 1:
+            return True 
+        
+        
+        temp = 1
+        
+        while temp < n:
+            
+            temp = temp*3
+            
+            if temp == n:
+                return True 
+            
+        return False 
