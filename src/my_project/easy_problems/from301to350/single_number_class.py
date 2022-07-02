6a87ed3400f33b30f0cb8c3c35a06b4c721b9b67
Ivan Penaloza <ivan1016017@gmail.com>
2022-07-02 07:29:46 -0500

    single number algorithm in python using xor and design patterns

--- src/my_project/easy_problems/from301to350/single_number_class.py
+++ src/my_project/easy_problems/from301to350/single_number_class.py
@@ -1,87 +1,66 @@
 from typing import List 
 from abc import ABC, abstractmethod
-
+from collections import defaultdict
 
 class Solution:
     
-    __name_of_instance = "Solution"
+    __name_of_instance = 'Solution'
     
     def __init__(self) -> None: 
         if Solution.__name_of_instance != "Solution":
-            raise Exception("This is a singleton instance")
+            raise Exception("This is a singleton class")
         else: 
             Solution.__name_of_instance = self 
             
     @staticmethod
-    def get_instance() -> None:
+    def get_instance() -> None: 
         print(Solution.__name_of_instance)
+    
+    def singleNumber(self, nums: List[int]) -> int:
+        
+        temp_dict = defaultdict(int)
+        
+        for num in nums:
+            temp_dict[num] += 1
             
-    def isHappy(self, n: int) -> bool:
+        for num in temp_dict:
+            if temp_dict[num] == 1:
+                return num 
         
-        def get_next(n:int) -> int:
-            total_sum = 0
-            while n > 0:
-                digit = n % 10
-                n = n // 10
-                total_sum += digit**2
-            return total_sum
-        
-        seen = list()
-        
-        while n != 1 and n not in seen:
-            seen.append(n)
-            n = get_next(n)
-            
-        return n == 1
+        return -1
     
-class SolutionTwo(object):
-    def isHappy(self, n):
-        """
-        :type n: int
-        :rtype: bool
-        """
-        def sqsum(num):
-            res = 0
-            while num > 0:
-                r = num % 10
-                res += r * r
-                num /= 10
-            return res
+class SolutionTwo: 
+    def singleNumber(self, nums: List[int]) -> int:
+        standard = 0
+        for num in nums:
+            standard = num ^ standard
         
-        rec = set()
-        while sqsum(n) not in rec:
-            summ = sqsum(n)
-            if summ == 1:
-                return True
-            else:
-                rec.add(summ)
-                n = summ
-        return False
+        return standard
     
     
-def factory(name: str = "SolutionOne"):
-    
+def factory(name:str = 'SolutionOne'): 
     localizers = {
-        "SolutionOne": Solution,
-        "SolutionTwo": SolutionTwo
+        'SolutionOne': Solution,
+        'SolutionTwo': SolutionTwo 
     }
     
     return localizers.get(name, Solution)()
 
-class WrittenText:
+class WrittenText: 
     
     def __init__(self,text:str) -> None: 
         self._text = text 
         
-    def render(self) -> str:
+    def render(self) -> str: 
         return self._text 
+    
     
 class UnderlineWrapper(WrittenText):
     
-    def __init__(self,wrappedClass) -> None:
+    def __init__(self,wrappedClass) -> None: 
         self._wrappedClass = wrappedClass
         
-    def render(self) -> str:
+    def render(self) -> str: 
         return "<u>{}</u>".format(self._wrappedClass.render())
     
 class ItaliclineWrapper(WrittenText):
@@ -92,7 +71,6 @@ class ItaliclineWrapper(WrittenText):
     def render(self) -> str:
         return "<i>{}</i>".format(self._wrappedClass.render()) 
     
-    
 class LogisticRegression:
     
     def __init__(self) -> None:
@@ -101,9 +79,9 @@ class LogisticRegression:
     def model_type(self) -> str:
         return 'Regression Model'
     
-class XGBoost:
+class XGBoost: 
     
-    def __init__(self,max_depth: int = None) -> None:
+    def __init__(self,max_depth: int = None) -> None: 
         self.name = 'XGBoost'
         self.max_depth = max_depth 
         
@@ -112,6 +90,7 @@ class XGBoost:
     
     def __str__(self) -> str: 
         return f"This is an instance of the XGBoost class with max_depth {self.max_depth}"
+    
     
 class Adapter: 
     
@@ -122,8 +101,9 @@ class Adapter:
     def __getattr__(self,attr):
         return getattr(self.obj,attr)
     
-    def original_dict(self): 
-        return self.obj.__dict__ 
+    def original_dict(self):
+        return self.obj.__dict__
+    
     
 class MlModel: 
     @abstractmethod
@@ -144,29 +124,33 @@ class LightGBM(MlModel):
     def is_fitted(self):
             print("LightGBM model fits the data") 
             
-class Production: 
+class Production:
     
-    def __init__(self, model: MlModel) -> None:
+    def __init__(self, model: MlModel) -> None: 
         self.model = model 
         self.is_fitted = False 
         
-    def predict(self):
         
-        if self.is_fitted:
+    def predict(self): 
+        
+        if self.is_fitted: 
             self.model.predict()
             self.is_fitted = False 
-        
+            
         else: 
             self.model.is_fitted()
-            self.is_fitted = True    
+            self.is_fitted = True 
 
 
-print(factory("sdfsd").isHappy(19))
-# print(factory("sdfsd").isHappy(19))
+print(factory('sdfsdf').singleNumber([1,1,2,2,7]))
+# print(factory('sdfsdf').singleNumber([1,1,2,2,7]))
 
 before = WrittenText("Eyes of the world.")
 
 after = UnderlineWrapper(ItaliclineWrapper(before))
+
+print(before.render())
+print(after.render())
 
 print(before.render())
 print(after.render())
@@ -189,5 +173,3 @@ light_gbm = LightGBM()
 ml_model = Production(light_gbm)
 ml_model.predict()
 ml_model.predict()
-
-    
