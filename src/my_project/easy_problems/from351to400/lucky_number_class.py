22d9aac05ee0dce953d3a2401e35771e6c97855c
ivan <ivan1016017@gmail.com>
2022-11-08 07:08:16 -0600

    lucky numbers algorithm with design patterns in python

---
+++ src/my_project/easy_problems/from351to400/lucky_number_class.py
@@ -0,0 +1,191 @@
+from typing import List, Union 
+from abc import ABC, abstractmethod
+
+class Solution:
+
+    __name_of_instance = 'SolutionOne'
+
+    def __init__(self) -> None: 
+        if Solution.__name_of_instance != 'SolutionOne':
+            raise Exception('This is a singleton class')
+        else: 
+            Solution.__name_of_instance = self 
+
+    @abstractmethod
+    def get_instance(self) -> None: 
+        print(Solution.__name_of_instance)
+
+    def luckyNumbers (self, matrix: List[List[int]]) -> List[int]:
+
+        ans = list()
+
+        len_matrix = len(matrix)
+        len_row = len(matrix[0])
+
+        for rows in matrix: 
+
+            col_index = 0
+
+            for j in range(len_row):
+                if  rows[j] < rows[col_index]:
+                    col_index = j 
+
+            found = False 
+
+            i = 0
+            while i < len_matrix and not found:
+                found = matrix[i][col_index] > rows[col_index]
+                i += 1
+
+            if not found:
+
+                ans.append(rows[col_index])
+
+        return ans
+
+
+class SolutionTwo:
+    def luckyNumbers (self, matrix: List[List[int]]) -> List[int]:
+        solution: List[int] = list()
+
+        def getColumn(matrix, j):
+            my_slide: List[int] = list()
+            for i in range(len(matrix)):
+                my_slide.append(matrix[i][j])
+            return my_slide 
+
+            return None 
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if matrix[i][j] == min([x for x in matrix[i][:]]) and matrix[i][j] == max(getColumn(matrix, j)): 
+                    solution.append(matrix[i][j])
+        return solution
+
+
+class WrittenText: 
+
+    def __init__(self,text:str) -> None: 
+        self._text = text 
+
+    def render(self) -> str: 
+        return self._text 
+
+class UnderlineWrapper(WrittenText):
+
+    def __init__(self,wrappedClass:WrittenText) -> None: 
+        self._wrappedClass = wrappedClass
+
+    def render(self) -> str: 
+        return '<u>{}</u>'.format(self._wrappedClass.render())
+
+class ItaliclineWrapper(WrittenText):
+    
+    def __init__(self, wrappedClass:WrittenText) -> None:
+        self._wrappedClass = wrappedClass
+        
+    def render(self) -> str:
+        return "<i>{}</i>".format(self._wrappedClass.render()) 
+
+class LogisticRegression:
+
+    def __init__(self) -> None: 
+        self.name = 'Logistic'
+
+    def model_type(self) -> str: 
+        return 'Regression Model'
+
+class XGBoost: 
+
+    def __init__(self,max_depth:int=None) -> None: 
+        self.name = 'XGBoost'
+        self.max_depth = max_depth 
+
+    def ml_model_type(self) -> str: 
+        return 'Ensemble Technique'      
+
+    def __str__(self) -> str: 
+        return f"This is an instance of the class XGBoost with max_depth {self.max_depth}"
+
+class Adapter:
+
+    def __init__(self,obj,**adapter_methods):
+        self.obj = obj 
+        self.__dict__.update(adapter_methods)
+
+    def __getattr__(self,attr):
+        return getattr(self.obj, attr)
+
+class MlModel:
+    @abstractmethod
+    def predict(self):
+        pass 
+
+class RandomForest(MlModel):
+
+    def predict(self):
+        print("Random forest predicted value")
+
+    def is_fitted(self):
+        print("Random forest model fits the data")
+
+class LightGBM(MlModel):
+    def predict(self):
+        print("LightGBM predicted value")
+        
+    def is_fitted(self):
+        print("LightGBM model fits the data")
+
+class Production(MlModel):
+
+    def __init__(self,model:MlModel):
+        self.model = model 
+        self.is_fitted = False 
+
+    def predict(self):
+        
+        if self.is_fitted:
+            self.model.predict()
+            self.is_fitted = False 
+        else: 
+            self.model.is_fitted()
+            self.is_fitted = True  
+
+def factory(name:str='SolutionOne') -> Union[Solution,SolutionTwo]:
+
+    localizers = {
+        'SolutionOne':Solution,
+        'SolutionTwo':SolutionTwo 
+    }
+
+    return localizers.get('asdfsd',Solution)()
+
+print(factory('sdfds').luckyNumbers(matrix = [[3,7,8],[9,11,13],[15,16,17]]))
+# print(factory('sdfds').luckyNumbers(matrix = [[3,7,8],[9,11,13],[15,16,17]]))
+
+
+before = WrittenText("Eyes of the world.")
+
+after = UnderlineWrapper(ItaliclineWrapper(before))
+
+print(before.render())
+print(after.render())
+
+logistic = LogisticRegression()
+xgboost = XGBoost()
+objects = list()
+
+objects.append(Adapter(logistic, model_type = logistic.model_type()))
+objects.append(Adapter(xgboost, model_type = xgboost.ml_model_type()))
+
+for obj in objects: 
+    print("A {0} model of type {1}".format(obj.name, obj.model_type))
+
+second_xgboost = XGBoost(7)
+print(second_xgboost)
+
+random_forest = RandomForest()
+
+ml_model = Production(random_forest)
+# this is a new comment
+ml_model.predict()
+ml_model.predict()  
