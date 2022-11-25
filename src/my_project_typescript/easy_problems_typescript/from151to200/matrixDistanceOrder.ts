14c241e9cb0989490c469807dc980dbc9e94a667
ivan <ivan1016017@gmail.com>
2022-11-25 07:24:05 -0600

    adding matrix distance algorithm in typescript

---
+++ src/my_project_typescript/easy_problems_typescript/from151to200/matrixDistanceOrder.ts
@@ -0,0 +1,19 @@
+function allCellsDistOrder(rows: number, cols: number, rCenter: number, cCenter: number): number[][] {
+
+    function distance(row:number, col:number):number{
+        return Math.abs(row-rCenter) + Math.abs(col-cCenter)
+    }
+
+    let answer: number[][] = []
+
+    for (let i = 0; i < rows;i++){
+        for (let j = 0; j < cols; j++){
+            answer.push([i,j])
+        }
+    }
+
+    answer = answer.sort((a,b) => distance(a[0],a[1]) < distance(b[0],b[1])?-1:1)
+
+    return answer;
+
+};
