14a31f654cf1595aec368ffabb36bc5bf6e7715a
ivan <ivan1016017@gmail.com>
2022-12-13 07:05:43 -0600

    adding trapping rain water algorithn in python and typescript with design patterns

---
+++ src/my_project/easy_problems/from351to400/trapping_rain_water_class.py
@@ -0,0 +1,189 @@
+from typing import List, Union
+from abc import ABC, abstractmethod
+
+class Solution:
+
+    __name_of_instance = 'SolutionOne'
+
+    def __init__(self) -> None: 
+        if Solution.__name_of_instance != 'SolutionOne':
+            raise Exception('This is a singleton class')
+        else: 
+            Solution.__name_of_instance = self 
+    
+    @abstractmethod
+    def get_instance(self) -> None: 
+        print(Solution.__name_of_instance)
+
+    def trap(self, height: List[int]) -> int:
+
+        solution = 0
+        l, r = 0, len(height)-1
+
+        while l < r: 
+            i = 1 
+
+            if height[l] < height[r]:
+                while height[l] > height[l+i]:
+                    solution += height[l]-height[l+i]
+                    i += 1
+                l += i
+            else:
+                while height[r] > height[r-i]:
+                    solution += height[r] - height[r-i]
+                    i += 1
+                r -= i 
+
+        return solution 
+
+class SolutionTwo:
+    def trap(self, height: List[int]) -> int:
+        n = len(height)
+        if n < 3:
+            return 0
+        left = [0] * n
+        right = [0] * n
+        left[0] = height[0]
+        right[n-1] = height[n-1]
+        for i in range(1, n):
+            left[i] = max(left[i-1], height[i])
+        for i in range(n-2, -1, -1):
+            right[i] = max(right[i+1], height[i])
+        res = 0
+        for i in range(1, n-1):
+            if height[i] < left[i] and height[i] < right[i]:
+                res += min(left[i], right[i]) - height[i]
+        return res
+
+class WrittenText:
+
+    def __init__(self,text:str) -> None: 
+        self._text = text 
+
+    def render(self) -> str: 
+        return self._text
+
+class UnderlineWrapper(WrittenText):
+
+    def __init__(self,wrappedClass:WrittenText) -> None: 
+        self._wrappedClass = wrappedClass
+
+    def render(self) -> str: 
+        return '<u>{}</u>'.format(self._wrappedClass.render())
+
+class ItaliclineWrapper(WrittenText):
+    
+    def __init__(self, wrappedClass:WrittenText) -> None:
+        self._wrappedClass = wrappedClass
+        
+    def render(self) -> str:
+        return "<i>{}</i>".format(self._wrappedClass.render()) 
+
+class LogisticRegression:
+
+    def __init__(self) -> None: 
+        self.name = 'Logistic'
+
+    def model_type(self) -> str: 
+        return 'Regression Model'
+
+class XGBoost: 
+
+    def __init__(self,max_depth:int=None) -> None: 
+        self.name = 'XGBoost'
+        self.max_depth = max_depth
+
+    def ml_model_type(self) -> str: 
+        return 'Ensemble Technique'
+
+    def __str__(self) -> str: 
+        return f'This is an instance of the class XGBoost with max_depth {self.max_depth}'
+
+class Adapter:
+
+    def __init__(self,obj,**adapter_methods):
+        self.obj = obj 
+        self.__dict__.update(adapter_methods)
+
+    def __getattr__(self,attr):
+        return getattr(self.obj,attr)
+
+    def original_dict(self):
+        return self.obj.__dict__ 
+
+class MlModel:
+    @abstractmethod
+    def predict(self):
+        pass 
+
+class RandomForest(MlModel):
+
+    def predict(self):
+        print('Random fores predicted value')
+
+    def is_fitted(self):
+        print("Random forest model fits the data")
+
+class LightGBM(MlModel):
+    def predict(self):
+        print("LightGBM predicted value")
+        
+    def is_fitted(self):
+        print("LightGBM model fits the data")
+
+class Production(MlModel):
+
+    def __init__(self,model:MlModel):
+        self.model = model 
+        self.is_fitted = False 
+
+    def predict(self):
+        
+        if self.is_fitted:
+            self.model.predict()
+            self.is_fitted = False 
+        else: 
+            self.model.is_fitted()
+            self.is_fitted = True   
+
+def factory(name:str='SolutionOne') -> Union[Solution,SolutionTwo]:
+
+    localizers = {
+        'SolutionOne':Solution,
+        'SolutionTwo':SolutionTwo 
+    }
+
+    return localizers.get(name,Solution)()
+
+print(factory('sdfsd').trap(height = [0,1,0,2,1,0,1,3,2,1,2,1]))
+# print(factory('sdfsd').trap(height = [0,1,0,2,1,0,1,3,2,1,2,1]))
+
+
+
+
+before = WrittenText("Eyes of the world.")
+
+after = UnderlineWrapper(ItaliclineWrapper(before))
+
+print(before.render())
+print(after.render())
+
+logistic = LogisticRegression()
+xgboost = XGBoost()
+objects = list()
+
+objects.append(Adapter(logistic, model_type = logistic.model_type()))
+objects.append(Adapter(xgboost, model_type = xgboost.ml_model_type()))
+
+for obj in objects: 
+    print("A {0} model of type {1}".format(obj.name, obj.model_type))
+
+second_xgboost = XGBoost(7)
+print(second_xgboost)
+
+random_forest = RandomForest()
+
+ml_model = Production(random_forest)
+# this is a new comment
+ml_model.predict()
+ml_model.predict()  
