fde06efdf9d856596703bd0a4b1f807e637de44c
ivan <ivan1016017@gmail.com>
2023-01-02 06:46:24 -0600

    adding decode strings algorithm in python and typescript

---
+++ src/my_project/medium_problems/from101to150/decode_strings_class.py
@@ -0,0 +1,198 @@
+from typing import List, Union 
+from abc import ABC, abstractmethod
+
+class Solution:
+
+    __name_of_instance = 'SolutionOne'
+
+    def __init__(self) -> None: 
+        if Solution.__name_of_instance != 'SolutionOne':
+            raise Exception('This is a singleton class')
+        else: 
+            Solution.__name_of_instance = self 
+
+    @abstractmethod
+    def get_instance(self) -> None: 
+        print(Solution.__name_of_instance)
+
+    def decodeString(self, s: str) -> str:
+        stack = []
+
+        for c in s:
+
+            if c == "]":
+                inner_s = ""
+
+                while stack[-1] != "[":
+                    inner_s = stack.pop() + inner_s
+
+                stack.pop()  # Remove the left bracket.
+
+                k_s = ""
+
+                while stack and stack[-1].isdigit():
+                    k_s = stack.pop() + k_s
+
+                k = int(k_s)
+
+                stack.append(k * inner_s)
+
+            else:
+                stack.append(c)
+
+        return "".join(stack)
+
+class SolutionTwo:
+    def decodeString(self, s: str) -> str:
+        res, stack = "", []
+        for char in s:
+            if char != ']':
+                stack.append(char)                 #append any char other than closing bracket to the stack
+            else:
+                currStr, multiplier = '', ''
+                while stack[-1] != '[':                      # this gives the current string till the last opening bracket
+                    currStr = stack.pop() + currStr
+                stack.pop()                                       # throw away the opening bracket, not needed
+                while len(stack) and stack[-1].isdigit():          # this gives the multiplier of the currentString
+                    multiplier = stack.pop() + multiplier
+                curr = int(multiplier) * currStr                # this generates the whole string
+                for c in curr:
+                    stack.append(c)                   # append the string back to the stack
+        return res + "".join(stack)
+
+
+class WrittenText: 
+
+    def __init__(self,text:str) -> None: 
+        self._text = text 
+
+    def render(self) -> str: 
+        return self._text
+
+class UnderlineWrapper(WrittenText):
+
+    def __init__(self,wrappedClass:WrittenText) -> None: 
+        self._wrappedClass = wrappedClass
+
+    def render(self) -> str: 
+        return '<u>{}</u>'.format(self._wrappedClass.render())
+
+class ItaliclineWrapper(WrittenText):
+    
+    def __init__(self, wrappedClass:WrittenText) -> None:
+        self._wrappedClass = wrappedClass
+        
+    def render(self) -> str:
+        return "<i>{}</i>".format(self._wrappedClass.render()) 
+
+class LogisticRegression:
+
+    def __init__(self) -> None: 
+        self.name = 'Logistic'
+
+    def model_type(self) -> str: 
+        return 'Regression Model'
+
+class XGBoost: 
+
+    def __init__(self,max_depth:int=None) -> None: 
+        self.name = 'XGBoost'
+        self.max_depth = max_depth 
+
+    def ml_model_type(self) -> str: 
+        return 'Ensemble Tecnique'
+
+    def __str__(self) -> str: 
+        return f'This is an instance of the class XGBoost with max_depth {self.max_depth}'
+
+class Adapter: 
+
+    def __init__(self,obj,**adapter_methods):
+        self.obj = obj 
+        self.__dict__.update(adapter_methods)
+
+    def __getattr__(self,attr): 
+        return getattr(self.obj,attr)
+
+    def original_dict(self): 
+        return self.obj.__dict__
+
+class MlModel: 
+    @abstractmethod
+    def predict(self): 
+        pass 
+
+class RandomForest(MlModel):
+
+    def predict(self):
+        print('Random forest predicted value')
+
+    def is_fitted(self):
+        print("Random forest model fits the data")
+
+class LightGBM(MlModel):
+    def predict(self):
+        print("LightGBM predicted value")
+        
+    def is_fitted(self):
+        print("LightGBM model fits the data")
+
+class Production(MlModel):
+
+    def __init__(self,model:MlModel):
+        self.model = model 
+        self.is_fitted = False 
+
+    def predict(self):
+        
+        if self.is_fitted:
+            self.model.predict()
+            self.is_fitted = False 
+        else: 
+            self.model.is_fitted()
+            self.is_fitted = True   
+
+def factory(name:str='SolutionOne') -> Union[Solution,SolutionTwo]:
+
+    localizers = {
+        'SolutionOne':Solution,
+        'SolutionTwo':SolutionTwo 
+    }
+
+    return localizers.get(name,Solution)()
+
+print(factory('sdfsd').decodeString(s = "3[a]2[bc]"))
+# print(factory('sdfsd').decodeString(s = "3[a]2[bc]"))
+
+
+
+
+before = WrittenText("Eyes of the world.")
+
+after = UnderlineWrapper(ItaliclineWrapper(before))
+
+print(before.render())
+print(after.render())
+
+logistic = LogisticRegression()
+xgboost = XGBoost()
+objects = list()
+
+objects.append(Adapter(logistic, model_type = logistic.model_type()))
+objects.append(Adapter(xgboost, model_type = xgboost.ml_model_type()))
+
+for obj in objects: 
+    print("A {0} model of type {1}".format(obj.name, obj.model_type))
+
+second_xgboost = XGBoost(7)
+print(second_xgboost)
+
+random_forest = RandomForest()
+
+ml_model = Production(random_forest)
+# this is a new comment
+ml_model.predict()
+ml_model.predict()  
+
+
+
