7266a4983dcf501cb45f850566836801890336fa
ivan <ivan1016017@gmail.com>
2023-01-21 08:54:06 -0600

    adding interview questions with design patterns

--- src/my_project/interviews/interview_1/revoke_digits.py
+++ src/my_project/interviews/interview_1/revoke_digits.py
@@ -3,22 +3,36 @@ from abc import ABC, abstractmethod
 
 class Solution:
     def removeKdigits(self, num: str, k: int) -> str:
-        if k >= len(num): return '0'
-        num = [l for l in num]
-        
-        while k > 0:
-            flag = True
-            for i in range(len(num)-1):
-                if str(num[i]) > str(num[i+1]):
-                    num.pop(i)
-                    k, flag = k - 1, False
-                    break
-                    
-            if flag == True and num[i+1] == num[-1]: 
-                num.pop(i+1)
-                k -= 1
-        
-        return str(int(''.join(num)))
+        """
+        key idea is remove those digit which is larger than its right digit,
+        :type num: str
+        :type k: int
+        :rtype: str
+        """
+
+        i, n = 0, len(num)
+        res = []
+        count = 0
+        while i < n - 1 and count < k:
+            # if num[i]> num[i+1] , num[i] is one need to be removed,so don't add it to res
+            # and we check the res from tail, pop those larger than num[i+1]
+            if num[i] > num[i + 1]:
+                count += 1
+                while len(res) > 0 and res[-1] > num[i + 1] and count < k:
+                    res.pop()
+                    count += 1
+            else:
+                res.append(num[i])
+            i += 1
+        res = "".join(res)
+        # if does not remove enough, we have remove count digit, still need to remove k-count digit,
+        #  currently, the res is in ascending order, we need to remove k-count-1 digit from tail of res
+        if count < k:
+            res = (res[:len(res) - (k - count - 1)]).lstrip('0')
+        else:
+            # if we have removed k digits, add the remaining of num to res
+            res = (res + num[i:]).lstrip('0')
+        return res if len(res) > 0 else "0"
 
 solution = Solution()
 
