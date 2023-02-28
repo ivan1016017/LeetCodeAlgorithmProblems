1bbd8fb3fd9f92e39394eec72b340409e11dd24a
ivan <ivan1016017@gmail.com>
2023-02-28 06:57:21 -0600

    adding algorithms intersection arrays and subtree with all depeest

---
+++ src/my_project/interviews/interviews_2_fb/subtree_with_all_deepest.py
@@ -0,0 +1,19 @@
+from typing import List, Union, Collection, Mapping, Optional
+from abc import ABC, abstractmethod
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def subtreeWithAllDeepest(self, root: TreeNode) -> TreeNode:
+
+        def deep(root):
+            if not root: return 0, None
+            l, r = deep(root.left), deep(root.right)
+            if l[0] > r[0]: return l[0] + 1, l[1]
+            elif l[0] < r[0]: return r[0] + 1, r[1]
+            else: return l[0] + 1, root
+        return deep(root)[1]
+        
