608cd1715e612a3880aa1e0cdc57cba54fdb7698
ivan <ivan1016017@gmail.com>
2023-03-21 07:02:04 -0600

    sample function to be deleted

--- src/my_project/interviews/msft_training_questions/trapping_rain_water.py
+++ src/my_project/interviews/msft_training_questions/trapping_rain_water.py
@@ -180,7 +180,12 @@ random_forest = RandomForest()
 ml_model = Production(random_forest)
 # this is a new comment
 ml_model.predict()
-ml_model.predict()  
+ml_model.predict() 
+
+# sample function to be deleted
+
+def to_be_deleted(name:str) -> str:
+    return name
 
 
 
