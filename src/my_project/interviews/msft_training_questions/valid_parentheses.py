57348111456b9a9ce052b708524f500eb78116bc
ivan <ivan1016017@gmail.com>
2023-03-22 07:14:51 -0600

    Revert "function to be deleted"
    This reverts commit c903c5d89e904c2e71f23ef5e96c3135e3f6ea65.

--- src/my_project/interviews/msft_training_questions/valid_parentheses.py
+++ src/my_project/interviews/msft_training_questions/valid_parentheses.py
@@ -192,7 +192,3 @@ ml_model = Production(random_forest)
 # this is a new comment
 ml_model.predict()
 ml_model.predict() 
-
-# function to be deleted
-def to_be_deleted(name:str='Nothing') -> str: 
-    return name
