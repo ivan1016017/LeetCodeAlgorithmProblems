5d349e98a33a4d16df7d2ddc3e1bd299b1fa842a
ivan <ivan1016017@gmail.com>
2023-03-24 07:17:26 -0600

    Revert "function to be deleted"
    This reverts commit 54e98ac432470b894175d68ae237aa6a81349903.

--- src/my_project/interviews/msft_training_questions/longest_palindrome.py
+++ src/my_project/interviews/msft_training_questions/longest_palindrome.py
@@ -198,7 +198,3 @@ ml_model = Production(random_forest)
 # this is a new comment
 ml_model.predict()
 ml_model.predict() 
-
-# function to be deleted
-def to_be_deleted(name:str='sample')-> str:
-    return name 
