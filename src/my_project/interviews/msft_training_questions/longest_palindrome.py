86c11ca8df90ff107a5f3938ab009fec383afd1f
ivan <ivan1016017@gmail.com>
2023-03-24 07:14:58 -0600

    adding longest palindrom algorithm in python with design patterns

---
+++ src/my_project/interviews/msft_training_questions/longest_palindrome.py
@@ -0,0 +1,200 @@
+from typing import List, Union, Collection, Mapping, Optional
+from abc import ABC, abstractmethod
+
+class Solution:
+
+    __name_of_instance = 'SolutionOne'
+
+    def __init__(self) -> None: 
+        if Solution.__name_of_instance != 'SolutionOne':
+            raise Exception('This is a singleton class')
+        else: 
+            Solution.__name_of_instance = self 
+
+    @abstractmethod
+    def get_instance(self) -> None: 
+        print(Solution.__name_of_instance)
+
+    def longestPalindrome(self, s):
+        answer = ''
+        
+        # check odd palindrome like aca
+        for i in range(len(s)):
+            temp, temp_len = self.get_palidrome(s,i,i)
+            if temp_len > len(answer):
+                answer = temp 
+
+        # check even palindrome like acca
+
+            temp, temp_len = self.get_palidrome(s,i,i+1)
+            if temp_len > len(answer):
+                answer = temp 
+
+        return  answer
+
+
+    def get_palidrome(self,s,l,r):
+        len_s = len(s)
+        while l>=0 and r < len_s and s[l] == s[r]:
+            l -= 1
+            r += 1
+
+        return [s[l+1:r],r-l]
+    
+
+class SolutionTwo:
+
+    def longestPalindromeTwo(self, s):
+        longest_palindrom = ''
+        dp = [[0]*len(s) for _ in range(len(s))]
+        #filling out the diagonal by 1
+        for i in range(len(s)):
+            dp[i][i] = True
+            longest_palindrom = s[i]
+            
+        # filling the dp table
+        for i in range(len(s)-1,-1,-1):
+                # j starts from the i location : to only work on the upper side of the diagonal 
+            for j in range(i+1,len(s)):  
+                if s[i] == s[j]:  #if the chars mathces
+                    # if len slicied sub_string is just one letter if the characters are equal, we can say they are palindomr dp[i][j] =True 
+                    #if the slicied sub_string is longer than 1, then we should check if the inner string is also palindrom (check dp[i+1][j-1] is True)
+                    if j-i ==1 or dp[i+1][j-1] is True:
+                        dp[i][j] = True
+                        # we also need to keep track of the maximum palindrom sequence 
+                        if len(longest_palindrom) < len(s[i:j+1]):
+                            longest_palindrom = s[i:j+1]
+                
+        return longest_palindrom
+    
+class WrittenText:
+
+    def __init__(self,text:str) -> None: 
+        self._text = text 
+
+    def render(self) -> str: 
+        return self._text
+
+class UnderlineWrapper(WrittenText): 
+
+    def __init__(self,wrappedClass:WrittenText) -> None: 
+        self._wrappedClass = wrappedClass
+
+    def render(self) -> str: 
+        return '<u>{}</u>'.format(self._wrappedClass.render())
+
+class ItaliclineWrapper(WrittenText):
+    
+    def __init__(self, wrappedClass:WrittenText) -> None:
+        self._wrappedClass = wrappedClass
+        
+    def render(self) -> str:
+        return "<i>{}</i>".format(self._wrappedClass.render()) 
+
+class LogisticRegression:
+
+    def __init__(self) -> None: 
+        self.name = 'Logistic'
+
+    def model_type(self) -> str: 
+        return 'Regression Model'
+
+class XGBoost: 
+
+    def __init__(self,max_depth:int=None) -> None: 
+        self.name = 'XGBoost'
+        self.max_depth = max_depth
+
+    def ml_model_type(self) -> str: 
+        return 'Ensemble Tecnique'
+
+    def __str__(self) -> str: 
+        return f"This is an instance of the class XGBoost with max_depth {self.max_depth}"
+
+class Adapter:
+
+    def __init__(self,obj,**adapter_methods):
+        self.obj = obj 
+        self.__dict__.update(adapter_methods)
+
+    def __getattr__(self,attr):
+        return getattr(self.obj,attr)
+
+    def original_dict(self):
+        return self.obj.__dict__
+
+class MlModel:
+    @abstractmethod
+    def predict(self):
+        pass
+
+class RandomForest(MlModel):
+
+    def predict(self):
+        print("Random forest predicted value")
+
+    def is_fitted(self):
+        print("Random forest model fits the data")
+
+class LightGBM(MlModel):
+    def predict(self):
+        print("LightGBM predicted value")
+        
+    def is_fitted(self):
+        print("LightGBM model fits the data")
+
+class Production(MlModel):
+
+    def __init__(self,model:MlModel):
+        self.model = model 
+        self.is_fitted = False 
+
+    def predict(self):
+        
+        if self.is_fitted:
+            self.model.predict()
+            self.is_fitted = False 
+        else: 
+            self.model.is_fitted()
+            self.is_fitted = True  
+
+def factory(name:str='SolutionOne') -> Union[Solution,SolutionTwo]:
+
+    localizers = {
+        'SolutionOne':Solution,
+        'SolutionTwo':SolutionTwo
+    }
+
+    return localizers.get(name,Solution)()
+
+
+
+
+
+#adding new line
+before = WrittenText("Eyes of the world.")
+
+after = UnderlineWrapper(ItaliclineWrapper(before))
+
+print(before.render())
+print(after.render())
+
+logistic = LogisticRegression()
+xgboost = XGBoost()
+objects = list()
+
+objects.append(Adapter(logistic, model_type = logistic.model_type()))
+objects.append(Adapter(xgboost, model_type = xgboost.ml_model_type()))
+
+for obj in objects: 
+    print("A {0} model of type {1}".format(obj.name, obj.model_type))
+
+second_xgboost = XGBoost(7)
+print(second_xgboost)
+
+random_forest = RandomForest()
+
+ml_model = Production(random_forest)
+# this is a new comment
+ml_model.predict()
+ml_model.predict() 
