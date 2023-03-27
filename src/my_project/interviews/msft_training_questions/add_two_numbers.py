e3e77933cd05afac8235dec889d430bd258c50eb
ivan <ivan1016017@gmail.com>
2023-03-27 07:21:06 -0600

    adding add two linked lists algorithm in python with design patterns

---
+++ src/my_project/interviews/msft_training_questions/add_two_numbers.py
@@ -0,0 +1,196 @@
+from typing import List, Union, Collection, Mapping, Optional
+from abc import ABC, abstractmethod
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+
+    __name_of_instance = 'SolutionOne'
+
+    def __init__(self) -> None: 
+        if Solution.__name_of_instance != 'SolutionOne':
+            raise Exception('This is a singleton class')
+        else: 
+            Solution.__name_of_instance = self 
+
+    @abstractmethod
+    def get_instance(self) -> None: 
+        print(self.__name_of_instance)
+
+    def addTwoNumbers(self, l1, l2):
+
+        head = runner = ListNode(0)
+        temp_sum = 0
+
+        while l1 or l2 or temp_sum >= 10:
+
+            runner.next = ListNode(0)
+            runner = runner.next 
+            temp_sum //=10
+
+            if l1:
+                temp_sum += l1.val 
+                l1 = l1.next 
+            if l2:
+                temp_sum += l2.val 
+                l2 = l2.next 
+
+            runner.val = temp_sum % 10
+
+        return head.next 
+    
+class SolutionTwo:
+    def addTwoNumbers(self, l1, l2):
+        dummy = ListNode()
+        current_node = dummy
+        carry = 0
+        while l1 or l2 or carry:
+            v1 = l1.val if l1 else 0
+            v2 = l2.val if l2 else 0
+            val = v1 + v2 + carry
+            carry = val // 10
+            val = val%10 
+            current_node.next = ListNode(val)
+            current_node = current_node.next
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+        return dummy.next
+    
+class WrittenText:
+
+    def __init__(self,text:str) -> None: 
+        self._text = text 
+
+    def render(self) -> str: 
+        return self._text 
+    
+class UnderlineWrapper(WrittenText):
+
+    def __init__(self,wrappedClass:WrittenText) -> None: 
+        self._wrappedClass = wrappedClass
+
+    def render(self) -> str: 
+        return '<u>{}</u>'.format(self._wrappedClass.render())
+    
+class ItaliclineWrapper(WrittenText):
+    
+    def __init__(self, wrappedClass:WrittenText) -> None:
+        self._wrappedClass = wrappedClass
+        
+    def render(self) -> str:
+        return "<i>{}</i>".format(self._wrappedClass.render()) 
+
+class LogisticRegression:
+
+    def __init__(self) -> None: 
+        self.name = 'Logistic'
+
+    def model_type(self) -> str: 
+        return 'Regression Model'
+    
+class XGBoost: 
+
+    def __init__(self,max_depth:int=None) -> None: 
+        self.name = 'XGBoost'
+        self.max_depth = max_depth 
+
+    def ml_model_type(self) -> str: 
+        return 'Ensemble Technique'
+    
+    def __str__(self) -> str: 
+        return f'This is an instance of the class XGBoost with max_depth {self.max_depth}'
+    
+class Adapter: 
+
+    def __init__(self,obj,**adapter_methods):
+        self.obj = obj 
+        self.__dict__.update(adapter_methods)
+
+    def __getattr__(self,attr):
+        return getattr(self.obj,attr)
+    
+    def original_dict(self):
+        return self.obj.__dict__
+    
+class MlModel:
+    @abstractmethod
+    def predict(self):
+        pass
+
+class RandomForest(MlModel):
+
+    def predict(self):
+        print("Random forest predicted value")
+
+    def is_fitted(self):
+        print("Random forest model fits the data")
+
+class LightGBM(MlModel):
+    def predict(self):
+        print("LightGBM predicted value")
+        
+    def is_fitted(self):
+        print("LightGBM model fits the data")
+
+class Production(MlModel):
+
+    def __init__(self,model:MlModel):
+        self.model = model 
+        self.is_fitted = False 
+
+    def predict(self):
+        
+        if self.is_fitted:
+            self.model.predict()
+            self.is_fitted = False 
+        else: 
+            self.model.is_fitted()
+            self.is_fitted = True  
+
+def factory(name:str='SolutionOne') -> Union[Solution,SolutionTwo]:
+
+    localizers = {
+        'SolutionOne':Solution,
+        'SolutionTwo':SolutionTwo
+    }
+
+    return localizers.get(name,Solution)()
+
+#adding new line
+before = WrittenText("Eyes of the world.")
+
+after = UnderlineWrapper(ItaliclineWrapper(before))
+
+print(before.render())
+print(after.render())
+
+logistic = LogisticRegression()
+xgboost = XGBoost()
+objects = list()
+
+objects.append(Adapter(logistic, model_type = logistic.model_type()))
+objects.append(Adapter(xgboost, model_type = xgboost.ml_model_type()))
+
+for obj in objects: 
+    print("A {0} model of type {1}".format(obj.name, obj.model_type))
+
+second_xgboost = XGBoost(7)
+print(second_xgboost)
+
+random_forest = RandomForest()
+
+ml_model = Production(random_forest)
+# this is a new comment
+ml_model.predict()
+ml_model.predict() 
+
+# time to be deleted
+def to_be_deleted(name:str='to be deleted') -> str:
+    return name
+
+
+        
