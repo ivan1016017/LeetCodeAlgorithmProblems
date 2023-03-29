eca0a8d396d6d6a496a2969122c7cc60a65cdc91
ivan <ivan1016017@gmail.com>
2023-03-29 06:15:09 -0600

    to be deleted

--- src/my_project/interviews/msft_training_questions/fizz_buzz.py
+++ src/my_project/interviews/msft_training_questions/fizz_buzz.py
@@ -176,3 +176,7 @@ ml_model = Production(random_forest)
 # this is a new comment
 ml_model.predict()
 ml_model.predict() 
+
+# function to be deleted
+def to_be_deleted(name:str='to be deleted') -> str:
+    return name 
