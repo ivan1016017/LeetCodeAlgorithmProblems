49fea964c430317a5b30a4f69aade2950582467e
ivan <ivan1016017@gmail.com>
2023-04-02 07:46:56 -0600

    function to be deleted

--- src/my_project/interviews/msft_training_questions/reverse_integer.py
+++ src/my_project/interviews/msft_training_questions/reverse_integer.py
@@ -174,3 +174,7 @@ ml_model = Production(random_forest)
 # this is a new comment
 ml_model.predict()
 ml_model.predict() 
+
+# function to be deleted
+def to_be_deleted(name:str='to be deleted') -> str: 
+    return name 
