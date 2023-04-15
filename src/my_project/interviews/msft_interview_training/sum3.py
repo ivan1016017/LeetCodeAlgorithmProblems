3bfb567d528350b586579974d57e72f313afe3c1
ivan <ivan1016017@gmail.com>
2023-04-15 10:58:53 -0600

    adding sum3 algorithm in python with design patterns

---
+++ src/my_project/interviews/msft_interview_training/sum3.py
@@ -0,0 +1,245 @@
+from typing import List, Union, Collection, Mapping, Optional
+from abc import ABC, abstractmethod
+
+
+class Solution:
+
+    __name_of_instance = 'SolutionOne'
+
+    def __int__(self) -> None: 
+        if Solution.__name_of_instance != 'SolutionOne':
+            raise Exception('This is a singleton class')
+        else: 
+            Solution.__name_of_instance = self 
+
+    @abstractmethod
+    def get_instance() -> None: 
+        print(Solution.__name_of_instance)
+
+
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+
+        answer = list()
+        positives = list()
+        negatives = list()
+        num_zeroes = 0
+
+        for num in nums:
+            if num == 0:
+                num_zeroes += 1
+            elif num > 0:
+                positives.append(num)
+            else: 
+                negatives.append(num)
+
+        s_positives = set(positives)
+        s_negatives = set(negatives)
+
+
+        for i in range(len(positives)):
+            for j in range(i+1,len(positives)):
+                k = positives[i] + positives[j]
+                if -k in s_negatives:
+                    mi = min(positives[i],positives[j])
+                    ma = max(positives[i],positives[j])
+                    answer.append((-k,mi,ma))
+
+        for i in range(len(negatives)):
+            for j in range(i+1,len(negatives)):
+                k = negatives[i] + negatives[j]
+                if -k in s_positives:
+                    mi = min(negatives[i],negatives[j])
+                    ma = max(negatives[i],negatives[j])
+                    answer.append((mi,ma,-k))
+
+        if num_zeroes >= 1:
+            for num in s_positives:
+                if -num in negatives:
+                    answer.append((-num,0,num))
+
+        if num_zeroes >=3:
+            answer.append((0,0,0))
+
+        return list(set(answer))
+    
+def threeSumTwo(self, nums: List[int]) -> List[List[int]]:
+
+	res = set()
+
+	#1. Split nums into three lists: negative numbers, positive numbers, and zeros
+	n, p, z = [], [], []
+	for num in nums:
+		if num > 0:
+			p.append(num)
+		elif num < 0: 
+			n.append(num)
+		else:
+			z.append(num)
+
+	#2. Create a separate set for negatives and positives for O(1) look-up times
+	N, P = set(n), set(p)
+
+	#3. If there is at least 1 zero in the list, add all cases where -num exists in N and num exists in P
+	#   i.e. (-3, 0, 3) = 0
+	if z:
+		for num in P:
+			if -1*num in N:
+				res.add((-1*num, 0, num))
+
+	#3. If there are at least 3 zeros in the list then also include (0, 0, 0) = 0
+	if len(z) >= 3:
+		res.add((0,0,0))
+
+	#4. For all pairs of negative numbers (-3, -1), check to see if their complement (4)
+	#   exists in the positive number set
+	for i in range(len(n)):
+		for j in range(i+1,len(n)):
+			target = -1*(n[i]+n[j])
+			if target in P:
+				res.add(tuple(sorted([n[i],n[j],target])))
+
+	#5. For all pairs of positive numbers (1, 1), check to see if their complement (-2)
+	#   exists in the negative number set
+	for i in range(len(p)):
+		for j in range(i+1,len(p)):
+			target = -1*(p[i]+p[j])
+			if target in N:
+				res.add(tuple(sorted([p[i],p[j],target])))
+
+	return res
+
+class WrittenText:
+
+    def __init__(self, text:str) -> None: 
+        self._text = text 
+
+    def render(self) -> str: 
+        return self._text
+    
+class UnderlineWrapper(WrittenText): 
+
+    def __init__(self,wrappedClass:WrittenText) -> None:
+        self._wrappedClass = wrappedClass
+
+    def render(self) -> str: 
+        return '<u>{}</u>'.format(self._wrappedClass.render())
+    
+class ItaliclineWrapper(WrittenText):
+    
+    def __init__(self, wrappedClass:WrittenText) -> None:
+        self._wrappedClass = wrappedClass
+        
+    def render(self) -> str:
+        return "<i>{}</i>".format(self._wrappedClass.render()) 
+
+class LogisticRegression:
+
+    def __init__(self) -> None: 
+        self.name = 'Logistic'
+
+    def model_type(self) -> str: 
+        return 'Regression Model'
+    
+class XGBoost: 
+
+    def __init__(self,max_depth:int=None):
+        self.name = 'XGBoost'
+        self.max_depth = max_depth 
+
+    def ml_model_type(self) -> str: 
+        return 'Ensemble Technique'
+    
+    def __str__(self) -> str: 
+        return f'This is an instance of the class XGBoost with max_depth {self.max_depth}'
+    
+class Adapter: 
+    
+    def __init__(self,obj,**adapter_methods): 
+        self.obj = obj 
+        self.__dict__.update(adapter_methods)
+
+    def __getattr__(self,attr):
+        return getattr(self.obj,attr)
+    
+
+class MlModel:
+    @abstractmethod
+    def predict(self):
+        pass 
+
+class RandomForest(MlModel):
+
+    def predict(self):
+        print('Random fores predicted value')
+
+    def is_fitted(self):
+        print("Random forest model fits the data")
+
+class LightGBM(MlModel):
+    def predict(self):
+        print("LightGBM predicted value")
+        
+    def is_fitted(self):
+        print("LightGBM model fits the data")
+
+class Production(MlModel):
+
+    def __init__(self,model:MlModel):
+        self.model = model 
+        self.is_fitted = False 
+
+    def predict(self):
+        
+        if self.is_fitted:
+            self.model.predict()
+            self.is_fitted = False 
+        else: 
+            self.model.is_fitted()
+            self.is_fitted = True   
+
+def factory(name:str='SolutionOne') -> Solution:
+
+    localizers = {
+        'SolutionOne':Solution,
+    }
+
+    return localizers.get(name,Solution)()
+
+# a = ListNode(1,ListNode(2))
+# b = ListNode(3,ListNode(4))
+
+# solution = Solution()
+# print(solution.addTwoNumbers(a,b).val)
+
+
+before = WrittenText("Eyes of the world.")
+
+after = UnderlineWrapper(ItaliclineWrapper(before))
+
+print(before.render())
+print(after.render())
+
+logistic = LogisticRegression()
+xgboost = XGBoost()
+objects = list()
+
+objects.append(Adapter(logistic, model_type = logistic.model_type()))
+objects.append(Adapter(xgboost, model_type = xgboost.ml_model_type()))
+
+for obj in objects: 
+    print("A {0} model of type {1}".format(obj.name, obj.model_type))
+
+second_xgboost = XGBoost(7)
+print(second_xgboost)
+
+random_forest = RandomForest()
+
+ml_model = Production(random_forest)
+# this is a new comment
+ml_model.predict()
+ml_model.predict()  
+
+
+
+    
+
