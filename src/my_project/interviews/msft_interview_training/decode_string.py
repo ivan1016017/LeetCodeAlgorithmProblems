8e6731e52394c87274b34a51e356ec5f070fc0ed
ivan <ivan1016017@gmail.com>
2023-04-19 06:34:13 -0600

    Revert "to be deleted"
    This reverts commit 628af8f9c932fbcb89f51283eb844cb4986c3289.

--- src/my_project/interviews/msft_interview_training/decode_string.py
+++ src/my_project/interviews/msft_interview_training/decode_string.py
@@ -181,7 +181,3 @@ ml_model = Production(random_forest)
 # this is a new comment
 ml_model.predict()
 ml_model.predict()  
-
-# to be deleted 
-def to_be_deleted(name:str='to be deleted') -> str: 
-    return name 
