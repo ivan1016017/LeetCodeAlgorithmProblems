ec2274faa1f665c19c9425fddf1a87f3e1e43c60
ivan <ivan1016017@gmail.com>
2023-07-08 07:31:31 -0600

    trapping rain water

---
+++ src/my_project/interviews/top_150_questions/trapping_rain_water.py
@@ -0,0 +1,227 @@
+from typing import List, Union, Collection, Mapping, Optional
+from abc import ABC, abstractmethod
+
+class Solution:
+
+    __name_of_instance = 'SolutionOne'
+
+    def __init__(self) -> None:
+        if Solution.__name_of_instance != 'SolutionOne':
+            raise Exception('This is a singleton class')
+        else: 
+            Solution.__name_of_instance = self 
+
+    @staticmethod
+    def get_instance() -> None: 
+        print(Solution.__name_of_instance)
+
+    def trap(self, height: List[int]) -> int:
+
+        n = len(height)
+        solution = 0
+
+        left = [0]*n 
+        right = [0]*n 
+
+        left[0] = height[0]
+        right[n-1] = height[n-1]
+
+        for i in range(1,n):
+            left[i] = max(left[i-1],height[i])
+
+        for i in range(1,n):
+            right[n-1-i] = max(right[n-i],height[n-1-i])
+
+        for i in range(1,n-1):
+            if left[i] > height[i] and height[i] < right[i]:
+                solution += min(left[i],right[i]) - height[i]
+
+        return solution
+    
+
+class SolutionTwo:
+    def trap(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        if height == []:
+            return 0
+
+        n = len(height)
+        max_left = [0]* n
+        max_right = [0]* n
+        ##print(max_left, height)
+        max_left[0] = 0
+        max_right[n-1] = 0
+        for i in range (1, n):
+            max_left[i] = max(max_left[i - 1], height[i-1])
+             
+        for i in range(n-2, -1, -1):
+           
+            max_right[i] = max(max_right[i + 1], height[i + 1])
+
+        output = 0
+        print(max_left, max_right)
+        
+        for i in range(n):
+		    
+            lower_boundary = min(max_left[i], max_right[i])
+            
+            max_trap_at_i = lower_boundary - height[i]
+            
+            if max_trap_at_i > 0:
+                output += max_trap_at_i
+                
+        return output
+    
+class WrittenText:
+
+    def __init__(self, text:str) -> None: 
+        self._text = text 
+
+    def render(self) -> str: 
+        return self._text
+    
+class UnderlineWrapper(WrittenText): 
+
+    def __init__(self,wrappedClass:WrittenText) -> None:
+        self._wrappedClass = wrappedClass
+
+    def render(self) -> str: 
+        return '<u>{}</u>'.format(self._wrappedClass.render())
+    
+class ItaliclineWrapper(WrittenText):
+    
+    def __init__(self, wrappedClass:WrittenText) -> None:
+        self._wrappedClass = wrappedClass
+        
+    def render(self) -> str:
+        return "<i>{}</i>".format(self._wrappedClass.render()) 
+
+class LogisticRegression:
+
+    def __init__(self) -> None: 
+        self.name = 'Logistic'
+
+    def model_type(self) -> str: 
+        return 'Regression Model'
+    
+class XGBoost: 
+
+    def __init__(self,max_depth:int=None):
+        self.name = 'XGBoost'
+        self.max_depth = max_depth 
+
+    def ml_model_type(self) -> str: 
+        return 'Ensemble Technique'
+    
+    def __str__(self) -> str: 
+        return f'This is an instance of the class XGBoost with max_depth {self.max_depth}'
+    
+class Adapter: 
+    
+    def __init__(self,obj,**adapter_methods): 
+        self.obj = obj 
+        self.__dict__.update(adapter_methods)
+
+    def __getattr__(self,attr):
+        return getattr(self.obj,attr)
+    
+
+class MlModel:
+    @abstractmethod
+    def predict(self):
+        pass 
+
+class RandomForest(MlModel):
+
+    def predict(self):
+        print('Random fores predicted value')
+
+    def is_fitted(self):
+        print("Random forest model fits the data")
+
+class LightGBM(MlModel):
+    def predict(self):
+        print("LightGBM predicted value")
+        
+    def is_fitted(self):
+        print("LightGBM model fits the data")
+
+class Production(MlModel):
+
+    def __init__(self,model:MlModel):
+        self.model = model 
+        self.is_fitted = False 
+
+    def predict(self):
+        
+        if self.is_fitted:
+            self.model.predict()
+            self.is_fitted = False 
+        else: 
+            self.model.is_fitted()
+            self.is_fitted = True   
+
+def factory(name:str='SolutionOne') -> Solution:
+
+    localizers = {
+        'SolutionOne':Solution,
+    }
+
+    return localizers.get(name,Solution)()
+
+# a = ListNode(1,ListNode(2))
+# b = ListNode(3,ListNode(4))
+
+# solution = Solution()
+# print(solution.addTwoNumbers(a,b).val)
+
+
+before = WrittenText("Eyes of the world.")
+
+after = UnderlineWrapper(ItaliclineWrapper(before))
+
+print(before.render())
+print(after.render())
+
+logistic = LogisticRegression()
+xgboost = XGBoost()
+objects = list()
+
+objects.append(Adapter(logistic, model_type = logistic.model_type()))
+objects.append(Adapter(xgboost, model_type = xgboost.ml_model_type()))
+
+for obj in objects: 
+    print("A {0} model of type {1}".format(obj.name, obj.model_type))
+
+second_xgboost = XGBoost(7)
+print(second_xgboost)
+
+random_forest = RandomForest()
+
+ml_model = Production(random_forest)
+# this is a new comment
+ml_model.predict()
+ml_model.predict()  
+
+
+# solution = Solution()
+
+# print(solution.calculate(s = "(100+(4+5+2)-3)+(6+8)")) 
+# print('*******************')
+# print(solution.calculate(s = " 2-1 + 2 "))
+
+# a = 2
+# b = 3
+# sign = 1 
+# result = a*sign*b 
+# print(result)
+# sign=-1
+# print(result)
+
+
+            
+        
+
