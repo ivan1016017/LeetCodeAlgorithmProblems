ca63d5e93f61c1e20b950a4c3f91806e1fcac46f
ivan <ivan1016017@gmail.com>
2023-10-08 07:02:16 -0600

    adding binaries

---
+++ src/my_project/interviews/top_150_questions/add_binary_updated_updated.py
@@ -0,0 +1,204 @@
+from typing import List, Union, Collection, Mapping, Optional
+from abc import ABC, abstractmethod
+
+class Solution:
+
+    __name_of_instance = 'SolutionOne'
+
+    def __init__(self) -> None:
+        if Solution.__name_of_instance != 'SolutionOne':
+            raise Exception('This is a singleton class')
+        else: 
+            Solution.__name_of_instance = self 
+
+    @staticmethod
+    def get_instance() -> None: 
+        print(Solution.__name_of_instance)
+
+    def addBinary(self, a: str, b: str) -> str:
+        return bin(int(a,2)+int(b,2))[2:]
+    
+class SolutionTwo:
+    def addBinary(self, a: str, b: str) -> str:
+
+        len_a = len(a)
+        len_b = len(b)
+
+        if len_a < len_b:
+            temp_a = '0' * (len_b-len_a)
+            a = temp_a + a 
+        if len_a > len_b:
+            temp_b = '0' * (len_a-len_b)
+            b = temp_b + b
+
+        decimal_number = self.fromBinaryToDecimal(a) + self.fromBinaryToDecimal(b)
+        return self.fromDecimalToBinary(decimal_number=decimal_number)
+
+
+    def fromBinaryToDecimal(self,binary_number:str) -> int: 
+        temp = 0
+        len_binary = len(binary_number)
+        for i in range(len_binary):
+            temp += int(binary_number[len_binary-1-i])*(2**(i))
+        return temp 
+
+    def fromDecimalToBinary(self,decimal_number:str) -> str: 
+        quotient = 1
+        remainder = 0
+        temp = ''
+
+        while quotient > 0:
+            quotient,remainder = decimal_number//2, decimal_number%2
+            temp = str(remainder) + temp 
+            decimal_number = quotient
+
+        return temp 
+    
+class WrittenText:
+
+    def __init__(self, text:str) -> None: 
+        self._text = text 
+
+    def render(self) -> str: 
+        return self._text
+    
+class UnderlineWrapper(WrittenText): 
+
+    def __init__(self,wrappedClass:WrittenText) -> None:
+        self._wrappedClass = wrappedClass
+
+    def render(self) -> str: 
+        return '<u>{}</u>'.format(self._wrappedClass.render())
+    
+class ItaliclineWrapper(WrittenText):
+    
+    def __init__(self, wrappedClass:WrittenText) -> None:
+        self._wrappedClass = wrappedClass
+        
+    def render(self) -> str:
+        return "<i>{}</i>".format(self._wrappedClass.render()) 
+
+class LogisticRegression:
+
+    def __init__(self) -> None: 
+        self.name = 'Logistic'
+
+    def model_type(self) -> str: 
+        return 'Regression Model'
+    
+class XGBoost: 
+
+    def __init__(self,max_depth:int=None):
+        self.name = 'XGBoost'
+        self.max_depth = max_depth 
+
+    def ml_model_type(self) -> str: 
+        return 'Ensemble Technique'
+    
+    def __str__(self) -> str: 
+        return f'This is an instance of the class XGBoost with max_depth {self.max_depth}'
+    
+class Adapter: 
+    
+    def __init__(self,obj,**adapter_methods): 
+        self.obj = obj 
+        self.__dict__.update(adapter_methods)
+
+    def __getattr__(self,attr):
+        return getattr(self.obj,attr)
+    
+
+class MlModel:
+    @abstractmethod
+    def predict(self):
+        pass 
+
+class RandomForest(MlModel):
+
+    def predict(self):
+        print('Random fores predicted value')
+
+    def is_fitted(self):
+        print("Random forest model fits the data")
+
+class LightGBM(MlModel):
+    def predict(self):
+        print("LightGBM predicted value")
+        
+    def is_fitted(self):
+        print("LightGBM model fits the data")
+
+class Production(MlModel):
+
+    def __init__(self,model:MlModel):
+        self.model = model 
+        self.is_fitted = False 
+
+    def predict(self):
+        
+        if self.is_fitted:
+            self.model.predict()
+            self.is_fitted = False 
+        else: 
+            self.model.is_fitted()
+            self.is_fitted = True   
+
+def factory(name:str='SolutionOne') -> Solution:
+
+    localizers = {
+        'SolutionOne':Solution,
+    }
+
+    return localizers.get(name,Solution)()
+
+# a = ListNode(1,ListNode(2))
+# b = ListNode(3,ListNode(4))
+
+# solution = Solution()
+# print(solution.addTwoNumbers(a,b).val)
+
+
+before = WrittenText("Eyes of the world.")
+
+after = UnderlineWrapper(ItaliclineWrapper(before))
+
+print(before.render())
+print(after.render())
+
+logistic = LogisticRegression()
+xgboost = XGBoost()
+objects = list()
+
+objects.append(Adapter(logistic, model_type = logistic.model_type()))
+objects.append(Adapter(xgboost, model_type = xgboost.ml_model_type()))
+
+for obj in objects: 
+    print("A {0} model of type {1}".format(obj.name, obj.model_type))
+
+second_xgboost = XGBoost(7)
+print(second_xgboost)
+
+random_forest = RandomForest()
+
+ml_model = Production(random_forest)
+# this is a new comment
+ml_model.predict()
+ml_model.predict()  
+
+
+# solution = Solution()
+
+# print(solution.calculate(s = "(100+(4+5+2)-3)+(6+8)")) 
+# print('*******************')
+# print(solution.calculate(s = " 2-1 + 2 "))
+
+# a = 2
+# b = 3
+# sign = 1 
+# result = a*sign*b 
+# print(result)
+# sign=-1
+# print(result)
+                
+
+
