1883fc94fbcf95417c348f30f72c2d8487548f97
ivan <ivan1016017@gmail.com>
2023-11-05 10:14:09 -0600

    updating test in leetcode

--- setup.py
+++ setup.py
@@ -1,21 +1,24 @@
-"""
-    Setup file for my_project.
-    Use setup.cfg to configure your project.
-
-    This file was generated with PyScaffold 4.0.1.
-    PyScaffold helps you to put up the scaffold of your new Python project.
-    Learn more under: https://pyscaffold.org/
-"""
+#!/usr/bin/env python
+import os
 from setuptools import setup
 
-if __name__ == "__main__":
-    try:
-        setup(use_scm_version={"version_scheme": "no-guess-dev"})
-    except:  # noqa
-        print(
-            "\n\nAn error occurred while building the project, "
-            "please ensure you have the most updated version of setuptools, "
-            "setuptools_scm and wheel with:\n"
-            "   pip install -U setuptools setuptools_scm wheel\n\n"
-        )
-        raise
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "mean",
+    version = "0.0.4",
+    author = "Ferdinand van Wyk",
+    description = ("An example of how to set up pytest and Sphinx."),
+    license = "GNU",
+    keywords = "example pytest tutorial sphinx",
+    packages=['src', 'tests'],
+    long_description=read('README.md'),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+    ],
+)
---
+++ tests/trial.py
@@ -0,0 +1,4 @@
+from src.my_project.easy_problems.from1to50.majority_element import Solution
+
+solution = Solution()
+solution.majorityElement(nums=[1,1,3,3,5,5,5,7,7,7,7])
