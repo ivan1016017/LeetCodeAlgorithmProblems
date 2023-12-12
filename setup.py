9551c16db8e24a35ede0f59a07739595928c90fc
ivan <ivan1016017@gmail.com>
2023-12-12 09:49:32 -0600

    updating pytest projects

--- setup.py
+++ setup.py
@@ -1,24 +1,3 @@
-#!/usr/bin/env python
-import os
 from setuptools import setup
 
-# Utility function to read the README file.
-# Used for the long_description.  It's nice, because now 1) we have a top level
-# README file and 2) it's easier to type in the README file than to put a raw
-# string in below ...
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
-
-setup(
-    name = "LeetCode_Algorithms",
-    version = "1.0",
-    author = "Ivan Dario Peñaloza Rojas",
-    description = ("Setting up Pytest"),
-    license = "MIT License",
-    keywords = "Pytest",
-    packages=['src', 'tests'],
-    long_description=read('README.md'),
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-    ],
-)
+setup()
