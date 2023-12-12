0b61578f1dc3db93e428301f8b2016398f0ae787
ivan <ivan1016017@gmail.com>
2023-12-12 09:13:16 -0600

    updating tests

---
+++ tests/test_150_questions_round_1/test_valid_parentheses.py
@@ -0,0 +1,10 @@
+import unittest
+from src.my_project.interviews.top_150_questions_round_1.\
+valid_parentheses import Solution
+
+class ValidParenthesesTestCase(unittest.TestCase):
+
+    def test_valid_parentheses(self):
+        solution = Solution()
+        output = solution.isValid(s = "()")
+        self.assertTrue(output)
