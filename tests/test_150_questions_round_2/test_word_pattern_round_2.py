e2f5dc9e07d7501f3bc204c65dcf674c2f3fd522
ivan <ivan1016017@gmail.com>
2024-01-12 05:07:31 -0600

    word pattern with unit tests

---
+++ tests/test_150_questions_round_2/test_word_pattern_round_2.py
@@ -0,0 +1,20 @@
+import unittest
+from src.my_project.interviews.top_150_questions_round_2.\
+word_pattern import Solution
+
+class WordPatternTestCase(unittest.TestCase):
+
+    def test_is_word_pattern(self):
+        solution = Solution()
+        output = solution.wordPattern(pattern="abba", s="dog cat cat dog")
+        self.assertTrue(output)
+
+    def test_is_no_word_pattern(self):
+            solution = Solution()
+            output = solution.wordPattern(pattern="abba", s="dog cat cat fish")
+            self.assertFalse(output)
+
+    def test_is_lenght_word_pattern(self):
+            solution = Solution()
+            output = solution.wordPattern(pattern="abcd", s="dog cat cat fish")
+            self.assertFalse(output)
