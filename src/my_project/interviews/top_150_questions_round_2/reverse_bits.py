382aa44ea93e1af764220bce38d66ed71f8326f8
ivan <ivan1016017@gmail.com>
2024-01-24 04:51:40 -0600

    reverse bits

---
+++ src/my_project/interviews/top_150_questions_round_2/reverse_bits.py
@@ -0,0 +1,9 @@
+from typing import List, Union, Collection, Mapping, Optional
+from abc import ABC, abstractmethod
+
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        return int((('{0:032b}'.format(n))[::-1]),2)
+    
+solution = Solution()
+print(solution.reverseBits(2))
