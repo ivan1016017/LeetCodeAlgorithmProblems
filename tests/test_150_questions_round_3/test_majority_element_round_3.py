6b3ad284a96be561129f9c73d6b5dfebbc571603
ivan <ivan1016017@gmail.com>
2024-02-04 05:06:08 -0600

    adding majority element

---
+++ tests/test_150_questions_round_3/test_majority_element_round_3.py
@@ -0,0 +1,15 @@
+import unittest
+from src.my_project.interviews.top_150_questions_round_3.\
+majority_element import Solution
+
+class MajorityElement(unittest.TestCase):
+
+    def test_is_majority_element(self):
+        solution = Solution()
+        output = solution.majorityElement(nums=[3,2,3])
+        self.assertEqual(3,output)
+
+    def test_is_no_majority_element(self):
+            solution = Solution()
+            output = solution.majorityElement(nums=[1,2,3])
+            self.assertEqual(-1,output) 
