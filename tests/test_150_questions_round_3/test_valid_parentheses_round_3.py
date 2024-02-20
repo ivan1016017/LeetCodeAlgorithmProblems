9b7e121000cab5c02fa5989f27e06ca436950f29
ivan <ivan1016017@gmail.com>
2024-02-20 05:00:07 -0600

    adding valid parentheses algo

---
+++ tests/test_150_questions_round_3/test_valid_parentheses_round_3.py
@@ -0,0 +1,21 @@
+import unittest
+from src.my_project.interviews.top_150_questions_round_3.\
+valid_parentheses import Solution
+
+class ValidParenthesesTestCase(unittest.TestCase):
+
+    def test_is_valid_parentheses(self):
+        solution = Solution()
+        output = solution.isValid(s='()')
+        self.assertTrue(output)
+
+    def test_is_no_valid_parentheses_no_match(self):
+        solution = Solution()
+        output = solution.isValid(s='(]')
+        self.assertFalse(output)
+
+
+    def test_is_no_valid_parentheses_non_empty_list(self):
+        solution = Solution()
+        output = solution.isValid(s='((')
+        self.assertFalse(output)
