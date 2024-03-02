408d2b22d72a1647a5e636822bd3198da68b2925
ivan <ivan1016017@gmail.com>
2024-03-02 05:02:58 -0600

    single number

---
+++ src/my_project/interviews/top_150_questions_round_3/single_number.py
@@ -0,0 +1,9 @@
+from typing import List, Union, Collection, Mapping, Optional
+from abc import ABC, abstractmethod
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        answer = 0
+        for num in nums:
+            answer ^= num 
+        return answer
---
+++ tests/test_150_questions_round_3/test_single_number_round_3.py
@@ -0,0 +1,10 @@
+import unittest
+from src.my_project.interviews.top_150_questions_round_3.\
+single_number import Solution
+
+class SingleNumberTestCase(unittest.TestCase):
+
+    def test_single_number(self):
+        solution = Solution()
+        output = solution.singleNumber(nums=[2,2,1])
+        self.assertEqual(1,output)
