8af4d3b62650a790d1f5f0e2128a8b3904053311
ivan <ivan1016017@gmail.com>
2024-03-15 05:06:54 -0600

    adding longest common prefix algo

---
+++ tests/test_150_questions_round_4/test_longest_common_prefix_round_4.py
@@ -0,0 +1,15 @@
+import unittest
+from src.my_project.interviews.top_150_questions_round_4.\
+longest_common_prefix import Solution
+
+class LongestCommonPrefixTestCase(unittest.TestCase):
+
+    def test_longest_common_prefix_empty_list(self):
+        solution = Solution()
+        output = solution.longestCommonPrefix(strs=[])
+        self.assertEqual("", output)
+
+    def test_longest_common_prefix_non_empty_list(self):
+        solution = Solution()
+        output = solution.longestCommonPrefix(strs=["flower","flow","flight"])
+        self.assertEqual("fl", output)
