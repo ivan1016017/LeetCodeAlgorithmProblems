99414a868e4e130ae97ead1f8bb12ccce229d7b6
ivan <ivan1016017@gmail.com>
2024-03-16 04:57:42 -0600

    fixing unit test 1 != 0

--- tests/test_150_questions_round_4/test_find_index_first_occurrence_round_4.py
+++ tests/test_150_questions_round_4/test_find_index_first_occurrence_round_4.py
@@ -7,7 +7,7 @@ class FindIndexFirstOccurrence(unittest.TestCase):
     def test_find_index_first_occurrence(self):
         solution = Solution()
         output = solution.strStr(haystack="sadbutsad", needle="sad")
-        self.assertEqual(1, output)
+        self.assertEqual(0, output)
 
     def test_no_find_index_first_occurrence(self):
         solution = Solution()
