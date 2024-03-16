91b8f5d3eb321f790ba652878367f0546674f0eb
ivan <ivan1016017@gmail.com>
2024-03-16 04:56:26 -0600

    adding find index first occurrence

---
+++ tests/test_150_questions_round_4/test_find_index_first_occurrence_round_4.py
@@ -0,0 +1,15 @@
+import unittest
+from src.my_project.interviews.top_150_questions_round_4.\
+find_index_first_occurence import Solution
+
+class FindIndexFirstOccurrence(unittest.TestCase):
+
+    def test_find_index_first_occurrence(self):
+        solution = Solution()
+        output = solution.strStr(haystack="sadbutsad", needle="sad")
+        self.assertEqual(1, output)
+
+    def test_no_find_index_first_occurrence(self):
+        solution = Solution()
+        output = solution.strStr(haystack="leetcode", needle="leeto")
+        self.assertEqual(-1, output)
