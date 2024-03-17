4d8990d153252c1deeefc8f5b9a288a47b74789e
ivan <ivan1016017@gmail.com>
2024-03-17 04:59:57 -0600

    adding valid palindrome algo

---
+++ src/my_project/interviews/top_150_questions_round_4/valid_palindrome.py
@@ -0,0 +1,22 @@
+from typing import List, Union, Collection, Mapping, Optional
+from abc import ABC, abstractmethod
+import re
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+
+        # transform to lowercase
+        s = s.lower()
+
+        # remove non-alphanumeric values from the string 
+        s = re.sub(pattern='[^a-zA-Z0-9]',repl='',string=s)
+
+        # find is_palindrome flag
+
+        len_s = len(s)
+
+        for i in range(len_s//2):
+            if s[i] != s[len_s-1-i]:
+                return False 
+            
+        return True 
