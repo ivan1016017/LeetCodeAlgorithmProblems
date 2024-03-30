13ab60cdeee46ed9673249ca52bfb9dde16453f8
ivan <ivan1016017@gmail.com>
2024-03-30 04:52:29 -0600

    adding inverted binary tree algo

---
+++ src/my_project/interviews/top_150_questions_round_4/invert_binary_tree.py
@@ -0,0 +1,22 @@
+from typing import List, Union, Collection, Mapping, Optional
+from abc import ABC, abstractmethod
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def invertTree(self, root: TreeNode) -> TreeNode:
+
+        try: 
+            root.val
+        except:
+            return root
+        
+        root.left, root.right = (self.invertTree(root.right), 
+                                 self.invertTree(root.left))
+        
+        return root
