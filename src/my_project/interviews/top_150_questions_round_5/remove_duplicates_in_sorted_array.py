c0a34916069b22f5b3a47eb60bdecd4f5dc33e72
ivan <ivan1016017@gmail.com>
2024-04-13 04:46:25 -0600

    adding removing duplicates algo

---
+++ src/my_project/interviews/top_150_questions_round_5/remove_duplicates_in_sorted_array.py
@@ -0,0 +1,18 @@
+from typing import List, Union, Collection, Mapping, Optional
+from abc import ABC, abstractmethod
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+
+        j = 0 
+        len_nums = len(nums)
+
+        for i in range(len_nums - 1):
+
+            if nums[i] != nums[i+1]:
+                nums[j] = nums[i]
+                j += 1
+
+        nums[j] = nums[len_nums - 1]
+
+        return j + 1
