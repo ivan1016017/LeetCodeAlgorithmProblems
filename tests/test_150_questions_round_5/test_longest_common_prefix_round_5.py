66ea90a2cc275c384ab2ffd692928c3fd270953f
ivan <ivan1016017@gmail.com>
2024-04-18 04:27:14 -0600

    adding longest common prefix

---
+++ tests/test_150_questions_round_5/test_longest_common_prefix_round_5.py
@@ -0,0 +1,17 @@
+import unittest
+from src.my_project.interviews.top_150_questions_round_5.\
+longest_common_prefix import Solution
+
+class LongestCommonPrefixTestCase(unittest.TestCase):
+
+    def test_longest_common_prefix_non_empty(self):
+        solution = Solution()
+        target = 'fl'
+        output = solution.longestCommonPrefix(strs=["flower","flow","flight"])
+        self.assertEqual(target, output)
+
+    def test_longest_common_prefix_empty(self):
+        solution = Solution()
+        target = ''
+        output = solution.longestCommonPrefix(strs=["a","b","c"])
+        self.assertEqual(target, output)
