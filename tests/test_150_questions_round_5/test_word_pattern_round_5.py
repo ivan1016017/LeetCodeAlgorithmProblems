e63760f2628a4588c6fd5a9cefb007659c5e6c37
ivan <ivan1016017@gmail.com>
2024-04-22 04:33:53 -0600

    adding word pattern

---
+++ tests/test_150_questions_round_5/test_word_pattern_round_5.py
@@ -0,0 +1,20 @@
+import unittest
+from src.my_project.interviews.top_150_questions_round_5.\
+word_pattern import Solution
+
+class WordPatternTestCase(unittest.TestCase):
+
+    def test_is_word_pattern(self):
+        solution = Solution()
+        output = solution.wordPattern(pattern="abba", s="dog cat cat dog")
+        self.assertTrue(output)
+
+    def test_is_no_word_pattern_case_one(self):
+        solution = Solution()
+        output = solution.wordPattern(pattern="aab", s="bcb")
+        self.assertFalse(output)
+
+    def test_is_no_word_pattern_case_two(self):
+        solution = Solution()
+        output = solution.wordPattern(pattern='aa', s='bc')
+        self.assertFalse(output)
