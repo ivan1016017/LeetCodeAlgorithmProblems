66abf47e5ac5dac0bb238cc460fb85097e6eb214
ivan <ivan1016017@gmail.com>
2024-05-05 04:21:04 -0600

    reverse bits

---
+++ tests/test_150_questions_round_5/test_reverse_bits_round_5.py
@@ -0,0 +1,10 @@
+import unittest
+from src.my_project.interviews.top_150_questions_round_5.\
+reverse_bits import Solution
+
+class RerverseBitsTestCase(unittest.TestCase):
+
+    def test_reverse_bits(self):
+        solution = Solution()
+        output = solution.reverseBits(n=7)
+        self.assertEqual(3758096384, output)
