784af15fee523f8cd6bceffd5f51069a8da55add
ivan <ivan1016017@gmail.com>
2024-05-05 04:21:31 -0600

    test1

--- tests/test_150_questions_round_5/test_reverse_bits_round_5.py
+++ tests/test_150_questions_round_5/test_reverse_bits_round_5.py
@@ -8,3 +8,6 @@ class RerverseBitsTestCase(unittest.TestCase):
         solution = Solution()
         output = solution.reverseBits(n=7)
         self.assertEqual(3758096384, output)
+
+def sample1():
+    pass 
