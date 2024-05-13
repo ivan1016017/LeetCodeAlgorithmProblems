85388f37a494402b4ef9e0b687c0e9b65bcb6146
ivan <ivan1016017@gmail.com>
2024-05-13 04:38:42 -0600

    test 2

--- tests/test_150_questions_round_6/test_merge_sorted_array_round_6.py
+++ tests/test_150_questions_round_6/test_merge_sorted_array_round_6.py
@@ -4,13 +4,16 @@ merge_sorted_array import Solution
 
 class MergeSortedArrayTestCase(unittest.TestCase):
 
-    def test_merge_sorted_array(self):
+    def test_merge_sorted_array_trial2(self):
         solution = Solution()
-        output = solution.merge(nums1=[1,2,3,0,0,0], m=3, nums2=[2,5,6], n=3)
-        target = [1,2,2,3,5,6]
+        output = solution.merge(nums1=[1,2,3,0,0,0], m=3, nums2=[2,5,7], n=3)
+        target = [1,2,2,3,5,7]
 
         for k, v in enumerate(target):
             self.assertEqual(v, output[k])
 
 def sample1():
     print('test 1')
+
+def sample2():
+    print('test 2')
