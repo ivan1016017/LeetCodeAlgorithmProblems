29301abcdfef70fafb1419317317a956ecca3130
ivan <ivan1016017@gmail.com>
2024-05-14 04:25:41 -0600

    testing 2

--- tests/test_150_questions_round_6/test_remove_element_round_6.py
+++ tests/test_150_questions_round_6/test_remove_element_round_6.py
@@ -4,10 +4,10 @@ remove_element import Solution
 
 class RemoveElementsTestCase(unittest.TestCase):
 
-    def test_remove_elements(self):
+    def test_remove_elements_trial_2(self):
         solution = Solution()
-        output = solution.removeElement(nums=[3,2,2,3], val=3)
-        target = 2
+        output = solution.removeElement(nums=[3,2,2,4], val=3)
+        target = 1
         return self.assertEqual(target, output)
     
 def sample1():
