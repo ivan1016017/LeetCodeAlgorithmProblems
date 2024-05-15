266b0068a5e3e298ad7730123fa799dd9289485b
ivan <ivan1016017@gmail.com>
2024-05-15 04:32:33 -0600

    Revert "testing"
    This reverts commit 7a2c6798e7a693341ba5edc90764ec7ae0b1cb58.

--- tests/test_150_questions_round_6/test_remove_duplicates_sorted_array_round_6.py
+++ tests/test_150_questions_round_6/test_remove_duplicates_sorted_array_round_6.py
@@ -4,11 +4,8 @@ remove_duplicates_sorted_array import Solution
 
 class RemoveDuplicatesTestCase(unittest.TestCase):
 
-    def test_remove_duplicates_trial_1(self):
+    def test_remove_duplicates(self):
         solution = Solution()
-        output = solution.removeDuplicates(nums = [1,1,3])
+        output = solution.removeDuplicates(nums = [1,1,2])
         target = 2 
         self.assertEqual(target, output)
-
-def sample():
-    print('testing')
