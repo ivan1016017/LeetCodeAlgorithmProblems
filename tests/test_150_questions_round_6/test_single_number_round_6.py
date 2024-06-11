66dfbfb511fafe2c3e254b67f14ef97310d45e6e
ivan <ivan1016017@gmail.com>
2024-06-11 04:29:59 -0600

    single number

---
+++ tests/test_150_questions_round_6/test_single_number_round_6.py
@@ -0,0 +1,11 @@
+import unittest
+from src.my_project.interviews.top_150_questions_round_6.\
+single_number import Solution
+
+class SingleNumberTestCase(unittest.TestCase):
+
+    def test_single_number(self):
+        solution = Solution()
+        output = solution.singleNumber(nums=[2,2,1])
+        target = 1
+        self.assertEqual(output, target)
