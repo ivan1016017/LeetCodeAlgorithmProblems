75635f7ac67813a158ff1479f467732597feff8a
ivan <ivan1016017@gmail.com>
2024-06-28 04:28:11 -0600

    ransome note

---
+++ src/my_project/interviews/top_150_questions_round_7/ransome_note.py
@@ -0,0 +1,11 @@
+from typing import List, Union, Collection, Mapping, Optional
+from abc import ABC, abstractmethod
+
+class Solution:
+    def canConstruct(self,ransomNote: str, magazine: str) -> bool:
+
+        for s in set(ransomNote):
+            if ransomNote.count(s) > magazine.count(s):
+                return False 
+            
+        return True 
