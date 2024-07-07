07a79337b0dd2bef0e57725e6c048d402fbbe9ae
ivan <ivan1016017@gmail.com>
2024-07-07 04:28:15 -0600

    max depth algo

---
+++ tests/test_150_questions_round_7/test_max_depth_tree_round_7.py
@@ -0,0 +1,19 @@
+import unittest
+from src.my_project.interviews.top_150_questions_round_7.\
+max_depth_tree import TreeNode, Solution
+
+class MaxDepthTreeTestCase(unittest.TestCase):
+
+    def test_max_depth_null(self):
+        solution = Solution()
+        tree = None
+        output = solution.maxDepth(root=tree)
+        target = 0
+        self.assertEqual(output, target)
+
+    def test_max_depth(self):
+        solution = Solution()
+        tree = TreeNode(1)
+        output = solution.maxDepth(root=tree)
+        target = 1
+        self.assertEqual(output, target)
