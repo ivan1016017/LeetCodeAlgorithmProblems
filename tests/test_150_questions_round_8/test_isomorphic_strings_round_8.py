e70a9d730e7bf040be014d3fb2ca8f3c6966d62e
ivan <ivan1016017@gmail.com>
2024-08-04 04:23:35 -0600

    isomorphic string algo passess all unit tests

---
+++ tests/test_150_questions_round_8/test_isomorphic_strings_round_8.py
@@ -0,0 +1,15 @@
+import unittest
+from src.my_project.interviews.top_150_questions_round_8\
+.isomorphic_strings import Solution
+
+class IsIsomorphicTestCase(unittest.TestCase):
+
+    def test_is_isomorphic(self):
+        solution = Solution()
+        output = solution.isIsomorphic(s="egg", t="add")
+        self.assertTrue(output)
+
+    def test_is_no_isomorphic(self):
+        solution = Solution()
+        output = solution.isIsomorphic(s="foo", t="bar")
+        self.assertFalse(output)
