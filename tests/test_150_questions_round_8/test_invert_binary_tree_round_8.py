92810f8beb2d995a8393b63435d88316ae4abfb3
ivan <ivan1016017@gmail.com>
2024-08-14 04:22:10 -0600

    adding invert binary tree algo

---
+++ tests/test_150_questions_round_8/test_invert_binary_tree_round_8.py
@@ -0,0 +1,14 @@
+import unittest
+from src.my_project.interviews.top_150_questions_round_8.\
+invert_binary_tree import TreeNode, Solution
+
+
+class InvertBinaryTreeTestCase(unittest.TestCase):
+
+    def test_binary_tree(self):
+        solution = Solution()
+        target = TreeNode(1, TreeNode(3), TreeNode(2))
+        output = solution.invertTree(TreeNode(1, TreeNode(2), TreeNode(3)))
+        self.assertEqual(target.val, output.val)
+        self.assertEqual(target.left.val, output.left.val)
+        self.assertEqual(target.right.val, output.right.val)
