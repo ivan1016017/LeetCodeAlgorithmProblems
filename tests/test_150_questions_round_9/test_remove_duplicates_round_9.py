3db46de706f60d92ea7b66a8b84a7815027804dd
ivan <ivan1016017@gmail.com>
2024-08-29 04:39:11 -0600

    adding removing duplicates algo

---
+++ tests/test_150_questions_round_9/test_remove_duplicates_round_9.py
@@ -0,0 +1,12 @@
+import unittest
+from src.my_project.interviews.top_150_questions_round_9\
+.remove_duplicates import Solution
+
+
+class RemoveDuplicatesTestCase(unittest.TestCase):
+
+    def test_remove_duplicates(self):
+        solution = Solution()
+        output = solution.removeDuplicates(nums=[1,1,2])
+        target = 2
+        self.assertEqual(output, target)
