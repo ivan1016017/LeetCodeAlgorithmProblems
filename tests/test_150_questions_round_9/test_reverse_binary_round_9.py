90b756698ecd73d332155b405aad2fe323cf151a
ivan <ivan1016017@gmail.com>
2024-09-23 04:47:43 -0600

    adding reverse bits algo

---
+++ tests/test_150_questions_round_9/test_reverse_binary_round_9.py
@@ -0,0 +1,11 @@
+import unittest
+from src.my_project.interviews.top_150_questions_round_9\
+.reverse_bits import Solution
+
+class ReverseBitsTestCase(unittest.TestCase):
+
+    def test_reverse_bits(self):
+        solution = Solution()
+        output = solution.reverseBits(2)
+        target = 1073741824
+        self.assertEqual(output, target)
