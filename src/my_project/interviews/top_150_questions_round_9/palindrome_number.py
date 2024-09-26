0c2acd291bd975d9bdcb45ae0ee86fa4508bb3a7
ivan <ivan1016017@gmail.com>
2024-09-26 04:33:50 -0600

    adding palindrome number algo

---
+++ src/my_project/interviews/top_150_questions_round_9/palindrome_number.py
@@ -0,0 +1,10 @@
+from typing import List, Union, Collection, Mapping, Optional
+from abc import ABC, abstractmethod
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+
+        str_x = str(x)
+
+        return str_x == str_x[::-1]
+
