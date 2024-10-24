7f5eac85a5902c819e0630f72647ae06507dd75f
ivan <ivan1016017@gmail.com>
2024-10-24 04:28:04 -0600

    adding symmetric tree

---
+++ tests/test_150_questions_round_10/test_symmetric_tree_round_10.py
@@ -0,0 +1,15 @@
+import unittest
+from src.my_project.interviews.top_150_questions_round_10\
+.symmetric_tree import TreeNode, Solution
+
+class SymmetricTreeTestCase(unittest.TestCase):
+
+    def test_is_symmetric(self):
+        solution = Solution()
+        output = solution.isSymmetric(TreeNode(1, TreeNode(7), TreeNode(7)))
+        self.assertTrue(output)
+
+    def test_is_no_symmetric(self):
+        solution = Solution()
+        output = solution.isSymmetric(TreeNode(1, TreeNode(2), TreeNode(3)))
+        self.assertFalse(output)
