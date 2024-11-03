6b994e68026df3aef80866eae5ba969a0d7b5938
ivan <ivan1016017@gmail.com>
2024-11-03 04:16:10 -0600

    adding pow x n algo

---
+++ src/my_project/interviews/top_150_questions_round_10/pow_x_n.py
@@ -0,0 +1,14 @@
+from typing import List, Union, Collection, Mapping, Optional
+from abc import ABC, abstractmethod
+
+class Solution:
+    def myPow(self, x, n):
+
+        if not n: 
+            return 1 
+        if n < 0: 
+            return self.myPow(1/x, -n)
+        elif n % 2: 
+            return x*self.myPow(x, n-1)
+        else: 
+            return self.myPow(x**2, n//2)
