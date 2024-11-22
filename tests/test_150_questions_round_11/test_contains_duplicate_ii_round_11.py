8d3bddc46467e86a2997af4a100abf865cb1fe64
ivan <ivan1016017@gmail.com>
2024-11-22 04:36:34 -0600

    adding contain duplicates

---
+++ tests/test_150_questions_round_11/test_contains_duplicate_ii_round_11.py
@@ -0,0 +1,15 @@
+import unittest
+from src.my_project.interviews.top_150_questions_round_11\
+.contains_duplicate_ii import Solution
+
+class ContainsDuplicatesTestCase(unittest.TestCase):
+
+    def test_contains_duplicates(self):
+        solution = Solution()
+        output = solution.containsNearbyDuplicate(nums=[1,2,3,1], k=3)
+        self.assertTrue(output)
+
+    def test_contains_no_duplicates(self):
+        solution = Solution()
+        output = solution.containsNearbyDuplicate(nums=[1,2,3,1,2,3], k=2)
+        self.assertFalse(output)
