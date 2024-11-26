d4fa74064052f2af97b71d5e35bce1f7e6456123
ivan <ivan1016017@gmail.com>
2024-11-26 04:37:25 -0600

    adding same tree algo

---
+++ tests/test_150_questions_round_11/test_same_tree_round_11.py
@@ -0,0 +1,19 @@
+import unittest
+from src.my_project.interviews.top_150_questions_round_11\
+.same_tree import TreeNode, Solution
+
+class SameTreeTestCase(unittest.TestCase):
+
+    def test_is_same_tree(self):
+        solution = Solution()
+        tree1 = TreeNode(1, TreeNode(2), TreeNode(3))
+        tree2 = TreeNode(1, TreeNode(2), TreeNode(3))
+        output = solution.isSameTree(p=tree1, q=tree2)
+        return self.assertTrue(output)
+    
+    def test_is_no_same_tree(self):
+        solution = Solution()
+        tree1 = TreeNode(1, TreeNode(2), TreeNode(3))
+        tree2 = TreeNode(1, TreeNode(3), TreeNode(2))
+        output = solution.isSameTree(p=tree1, q=tree2)
+        return self.assertFalse(output)
