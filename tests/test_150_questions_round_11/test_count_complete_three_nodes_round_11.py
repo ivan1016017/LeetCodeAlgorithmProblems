5d91c1a4b91dfa49354e9702024a31939eaf088b
ivan <ivan1016017@gmail.com>
2024-11-30 04:28:38 -0600

    adding count complete nodes

---
+++ tests/test_150_questions_round_11/test_count_complete_three_nodes_round_11.py
@@ -0,0 +1,20 @@
+import unittest
+from src.my_project.interviews.top_150_questions_round_11\
+.count_complete_three_nodes import TreeNode, Solution
+
+class CountNodesTestCase(unittest.TestCase):
+
+    def test_count_none(self):
+        solution = Solution()
+        tree = None 
+        output = solution.countNodes(root=tree)
+        self.assertEqual(0, output)
+
+
+    def test_count_non_empty_tree(self):
+        solution = Solution()
+        tree = TreeNode(1, TreeNode(2), TreeNode(3))
+        output = solution.countNodes(root=tree)
+        self.assertEqual(3, output)
+
+        
