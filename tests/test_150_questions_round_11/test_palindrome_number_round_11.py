bc0a7b86812c4bf81fcd1799363e5309f9e5f02e
ivan <ivan1016017@gmail.com>
2024-12-05 04:32:24 -0600

    adding palindrome number

---
+++ tests/test_150_questions_round_11/test_palindrome_number_round_11.py
@@ -0,0 +1,15 @@
+import unittest
+from src.my_project.interviews.top_150_questions_round_11\
+.palindrome_number import Solution
+
+class PalindromeNumberTestCase(unittest.TestCase):
+
+    def test_is_palindrome_number(self):
+        solution = Solution()
+        output = solution.isPalindrome(x=121)
+        self.assertTrue(output)
+
+    def test_is_no_palindrome_number(self):
+        solution = Solution()
+        output = solution.isPalindrome(x=123)
+        self.assertFalse(output)
