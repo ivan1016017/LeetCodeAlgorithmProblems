9326620f16ca92251e305542f86194517a25db9c
ivan <ivan1016017@gmail.com>
2025-01-04 04:31:32 -0600

    adding path sum

---
+++ tests/test_150_questions_round_12/test_path_sum_round_12.py
@@ -0,0 +1,17 @@
+import unittest
+from src.my_project.interviews.top_150_questions_round_12\
+.path_sum import Solution, TreeNode
+
+class HasPathSumTestCase(unittest.TestCase):
+
+    def test_is_path_sum(self):
+        solution = Solution()
+        tree = TreeNode(1, TreeNode(2), TreeNode(3))
+        output = solution.hasPathSum(root=tree, targetSum=3)
+        self.assertTrue(output)
+
+    def test_is_no_path_sum(self):
+            solution = Solution()
+            tree = TreeNode(1, TreeNode(2), TreeNode(3))
+            output = solution.hasPathSum(root=tree, targetSum=10)
+            self.assertFalse(output)
