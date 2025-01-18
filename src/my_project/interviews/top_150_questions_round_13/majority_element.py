f5d91c11cc8f32517159485f9368f1b856e38445
ivan <ivan1016017@gmail.com>
2025-01-18 04:20:32 -0600

    majority element algo

---
+++ src/my_project/interviews/top_150_questions_round_13/majority_element.py
@@ -0,0 +1,22 @@
+from typing import List, Union, Collection, Mapping, Optional
+from abc import ABC, abstractmethod
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+
+        dic_answer = dict()
+        len_nums = len(nums)
+
+        for i in range(len_nums):
+
+            if nums[i] not in dic_answer: 
+                dic_answer[nums[i]] = 1
+            else:
+                dic_answer[nums[i]] += 1
+
+            if dic_answer[nums[i]] > len_nums//2:
+                return nums[i]
+            else: 
+                pass 
+
+        return -1
