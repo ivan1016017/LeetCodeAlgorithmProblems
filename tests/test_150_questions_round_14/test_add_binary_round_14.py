35c16d44de1366585e1012924b9ef0f492fd78dc
ivan <ivan1016017@gmail.com>
2025-03-19 04:34:30 -0600

    adding add binary

---
+++ tests/test_150_questions_round_14/test_add_binary_round_14.py
@@ -0,0 +1,11 @@
+import unittest
+from src.my_project.interviews.top_150_questions_round_14\
+.add_binary import Solution
+
+class AddBinaryTestCase(unittest.TestCase):
+
+    def test_add_binary(self):
+        solution = Solution()
+        output = solution.addBinary(a="11", b="1")
+        target = "100"
+        self.assertEqual(output, target)
