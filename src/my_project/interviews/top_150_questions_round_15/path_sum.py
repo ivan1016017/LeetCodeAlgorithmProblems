2ef1716bcf1bb50341466d0c6199eef20f267e01
ivan <ivan1016017@gmail.com>
2025-04-21 04:24:57 -0600

    adding algo

---
+++ src/my_project/interviews/top_150_questions_round_15/path_sum.py
@@ -0,0 +1,21 @@
+from typing import List, Union, Collection, Mapping, Optional
+from abc import ABC, abstractmethod
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+
+    def hasPathSum(self, root: TreeNode, targetSum: int) -> bool:
+
+        if not root: 
+            return False 
+        if not root.left and not root.right and root.val == targetSum: 
+            return True 
+        else: 
+            temp_target = targetSum - root.val 
+            return self.hasPathSum(root.left, temp_target) \
+                   or self.hasPathSum(root.right, temp_target)
