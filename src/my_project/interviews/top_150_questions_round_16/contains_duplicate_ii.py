3a2b47c35df55c2b071d5c441def13406bbfd165
ivan <ivan1016017@gmail.com>
2025-05-19 04:30:01 -0600

    adding algo

---
+++ src/my_project/interviews/top_150_questions_round_16/contains_duplicate_ii.py
@@ -0,0 +1,20 @@
+from typing import List, Union, Collection, Mapping, Optional
+from abc import ABC, abstractmethod
+from collections import defaultdict
+
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+
+        answer_dic = defaultdict(int)
+        len_nums = len(nums)
+
+        for i in range(len_nums):
+
+            answer_dic[nums[i]] += 1 
+
+            if i > k: 
+                answer_dic[nums[i - (k+1)]] -= 1
+            if answer_dic[nums[i]] > 1:
+                return True 
+            
+        return False
