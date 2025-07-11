c6e6aed0aedada0281141f3830e5a40ca2ed020b
ivan <ivan1016017@gmail.com>
2025-07-11 04:29:28 -0600

    adding algo

---
+++ tests/test_150_questions_round_18/test_merge_sorted_array_round_18.py
@@ -0,0 +1,12 @@
+import unittest
+from src.my_project.interviews.top_150_questions_round_18\
+.merge_sorted_array import Solution
+
+class MergeSortedArrayTestCase(unittest.TestCase):
+
+    def test_merge_sorted_array(self):
+        solution = Solution()
+        output = solution.merge(nums1=[1,2,3,0,0,0], m=3, nums2=[2,5,6], n=3)
+        target = [1,2,2,3,5,6]
+        for k, v in enumerate(target):
+            self.assertEqual(output[k], v)
