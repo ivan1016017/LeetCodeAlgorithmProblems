9ca229561656bdfb6a66498251e43757c7f54213
ivan <ivan1016017@gmail.com>
2025-07-17 04:26:23 -0600

    adding algo

---
+++ src/my_project/interviews/top_150_questions_round_18/length_of_last_word.py
@@ -0,0 +1,9 @@
+from typing import List, Union, Collection, Mapping, Optional
+from abc import ABC, abstractmethod
+
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+
+        lst_s = s.split()
+
+        return len(lst_s[-1])
