f4592c9befb9c1e803d03bae68db448f21fcb44a
ivan <ivan1016017@gmail.com>
2025-07-24 04:31:49 -0600

    adding algo

---
+++ src/my_project/interviews/top_150_questions_round_18/word_pattern.py
@@ -0,0 +1,21 @@
+from typing import List, Union, Collection, Mapping, Optional
+from abc import ABC, abstractmethod
+
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+
+        dic_answer = dict()
+
+        words = s.split(' ')
+
+        if len(words) != len(pattern) or len(set(words)) != len(set(pattern)):
+            return False 
+        else:
+            for k, v in enumerate(words):
+                if v in dic_answer:
+                    if dic_answer[v] != pattern[k]:
+                        return False
+                else: 
+                    dic_answer[v] = pattern[k]
+
+            return True
