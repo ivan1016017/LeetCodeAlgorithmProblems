28756b2803fa2c1e302df014f89fd15d283a40ed
ivan <ivan1016017@gmail.com>
2025-09-24 04:27:03 -0600

    adding algo

---
+++ tests/test_150_questions_round_20/test_best_time_to_buy_stock_round_20.py
@@ -0,0 +1,11 @@
+import unittest
+from src.my_project.interviews.top_150_questions_round_20\
+.best_time_to_buy_stock import Solution
+
+class BestTimeToSellStockTestCase(unittest.TestCase):
+
+    def test_best_time_to_sell_stock(self):
+        solution = Solution()
+        output = solution.maxProfit(prices=[7,1,5,3,6,4])
+        target = 5 
+        self.assertEqual(output, target)
