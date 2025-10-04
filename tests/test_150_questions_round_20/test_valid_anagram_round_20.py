5d987c41e4016857e35344157580e4177d41eae4
ivan <ivan1016017@gmail.com>
2025-10-04 04:28:57 -0600

    adding algo

---
+++ tests/test_150_questions_round_20/test_valid_anagram_round_20.py
@@ -0,0 +1,16 @@
+import unittest
+from src.my_project.interviews.top_150_questions_round_20\
+.valid_anagram import Solution
+
+class ValidAnagramTestCase(unittest.TestCase):
+
+    def test_is_valid_anagram(self):
+        solution = Solution()
+        output = solution.isAnagram(s="anagram", t="nagaram")
+        self.assertTrue(output)
+
+
+    def test_is_no_valid_anagram(self):
+        solution = Solution()
+        output = solution.isAnagram(s="rat", t="car")
+        self.assertFalse(output)
