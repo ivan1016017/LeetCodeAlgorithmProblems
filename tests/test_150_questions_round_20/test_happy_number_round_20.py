0c9945f41c3489512a3ae27ddca4ae2667d8ac02
ivan <ivan1016017@gmail.com>
2025-10-06 04:29:47 -0600

    adding algo

---
+++ tests/test_150_questions_round_20/test_happy_number_round_20.py
@@ -0,0 +1,15 @@
+import unittest
+from src.my_project.interviews.top_150_questions_round_20\
+.happy_number import Solution
+
+class HappyNumberTestCase(unittest.TestCase):
+
+    def test_is_happy_number(self):
+        solution = Solution()
+        output = solution.isHappy(n=19)
+        self.assertTrue(output)
+
+    def test_is_no_happy_number(self):
+        solution = Solution()
+        output = solution.isHappy(n=2)
+        self.assertFalse(output)
