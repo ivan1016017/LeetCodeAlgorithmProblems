cd5e91aa6416172d058b639ca7d11cfe12bb6494
ivan <ivan1016017@gmail.com>
2025-11-02 11:57:19 -0600

    adding new algo

---
+++ src/my_project/interviews/amazon_high_frequency_23/round_2/minimum_adjacent_swapt_to_make_valid_array.py
@@ -0,0 +1,16 @@
+from typing import List, Union, Collection, Mapping, Optional
+from abc import ABC, abstractmethod
+
+class Solution:
+    def minimumSwaps(self, nums: List[int]) -> int:
+
+        min_num = min(nums)
+        max_num = max(nums)
+
+        id_min = nums.index(min_num)
+
+        new_nums = [min_num] + nums[:id_min] + nums[id_min+1:]
+
+        id_max = new_nums[::-1].index(max_num)
+
+        return id_min + id_max
