0b9885d50b7bc6ee831795c2ba84c1169022867f
ivan <ivan1016017@gmail.com>
2025-11-09 04:20:11 -0600

    adding algo

---
+++ src/my_project/interviews/amazon_high_frequency_23/round_3/lowest_common_ancestor_binary_tree_iv.py
@@ -0,0 +1,29 @@
+from typing import List, Union, Collection, Mapping, Optional
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: TreeNode, nodes: List[TreeNode]) -> 'TreeNode':
+
+        node_set = set(nodes)
+
+        def dfs(node: TreeNode):
+
+            if not node or node in node_set:
+                return node 
+            
+            left = dfs(node.left)
+            right = dfs(node.right)
+
+            if left and right:
+                return node
+            
+            return left if left else right
+        
+        return dfs(root)
+        
