e2652a8477f771913ba41918ef5e67b3cae57416
ivan <ivan1016017@gmail.com>
2025-11-18 04:23:47 -0600

    adding algo

---
+++ src/my_project/interviews/amazon_high_frequency_23/round_3/the_kth_factor.py
@@ -0,0 +1,22 @@
+class Solution:
+    def kthFactor(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: int
+        """
+
+        
+        for i in range(1, n+1):
+            
+            if n % i == 0: 
+                k -= 1
+
+                if k == 0:
+                    return i
+        
+        return -1
+
+    
+
+        
