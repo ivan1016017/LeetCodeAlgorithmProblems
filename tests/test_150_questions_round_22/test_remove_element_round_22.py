6e6178ffa7f3a90712da1ced1ffac883b92e978a
ivan <ivan1016017@gmail.com>
2025-11-28 04:34:02 -0600

    adding algos

---
+++ tests/test_150_questions_round_22/test_remove_element_round_22.py
@@ -0,0 +1,11 @@
+import unittest
+from src.my_project.interviews.top_150_questions_round_22\
+.remove_element import Solution
+
+class RemoveElementTestCase(unittest.TestCase):
+
+    def test_remove_element(self):
+        solution = Solution()
+        output = solution.removeElement(nums=[3,2,2,3], val=3)
+        target = 2
+        self.assertEqual(output, target)
