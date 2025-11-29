35a0fd7fad26ff0bebd638f402301a988cfe3c01
ivan <ivan1016017@gmail.com>
2025-11-29 04:33:02 -0600

    adding algo

---
+++ src/my_project/interviews/top_150_questions_round_22/remove_duplicates_ii.py
@@ -0,0 +1,26 @@
+from typing import List, Union, Collection, Mapping, Optional
+from abc import ABC, abstractmethod
+
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
+        n = len(nums)
+        
+        if n < 3:
+            return n
+        
+        i , j = 1, 2
+        
+        while j < n:
+            if nums[i-1] != nums[j]:
+                i += 1
+            
+            
+            nums[i] = nums[j]
+            j+= 1
+        
+        return i+1
