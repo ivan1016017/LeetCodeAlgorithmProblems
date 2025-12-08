053402d25b21f746f01a9196f3826a2a1a9c7791
ivan <ivan1016017@gmail.com>
2025-12-08 04:04:06 -0600

    adding algo

---
+++ tests/test_150_questions_round_22/test_19_length_last_word_round_22.py
@@ -0,0 +1,11 @@
+import unittest
+from src.my_project.interviews.top_150_questions_round_22\
+.ex_19_length_last_word import Solution
+
+class LengthLastWordTestCase(unittest.TestCase):
+
+    def test_length_last_word(self):
+        solution = Solution()
+        output = solution.lengthOfLastWord(s="Hello World")
+        target = 5 
+        self.assertEqual(output, target)
