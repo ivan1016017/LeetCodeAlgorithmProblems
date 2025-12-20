c73fdc9568e8f7c276f073d27c9c2ddc9645d754
ivan <ivan1016017@gmail.com>
2025-12-20 03:57:42 -0600

    adding algo

--- src/my_project/interviews/amazon_high_frequency_23/common_algos/valid_palindrome_round_1.py
+++ src/my_project/interviews/amazon_high_frequency_23/common_algos/valid_palindrome_round_1.py
@@ -12,12 +12,11 @@ class Solution:
         s = re.sub(pattern=r'[^a-zA-Z0-9]', repl='', string=s)
 
         # Determine if s is palindrome or not
-
         len_s = len(s)
 
         for i in range(len_s//2):
-
+            
             if s[i] != s[len_s - 1 - i]:
                 return False 
             
-        return True
+        return True 
