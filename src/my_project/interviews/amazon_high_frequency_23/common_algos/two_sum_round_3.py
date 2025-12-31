d0c3387b15e74093d151e692b74d63cc650e35d7
ivan <ivan1016017@gmail.com>
2025-12-31 05:22:06 -0600

    adding algo

---
+++ src/my_project/interviews/amazon_high_frequency_23/common_algos/two_sum_round_3.py
@@ -0,0 +1,16 @@
+from typing import List, Union, Collection, Mapping, Optional
+from abc import ABC, abstractmethod
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+        answer = dict()
+
+        for k, v in enumerate(nums):
+
+            if v in answer:
+                return [answer[v], k]
+            else: 
+                answer[target - v] = k
+
+        return []
