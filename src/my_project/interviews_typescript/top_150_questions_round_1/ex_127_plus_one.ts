e8e1f6739bea68205780ea55addc1f26eeca42b0
ivan <ivan1016017@gmail.com>
2026-04-11 04:36:56 -0600

    adding updates

---
+++ src/my_project/interviews_typescript/top_150_questions_round_1/ex_127_plus_one.ts
@@ -0,0 +1,10 @@
+function plusOne(digits: number[]): number[] {
+    for (let i = digits.length - 1; i >= 0; i--) {
+        if (digits[i] !== 9) {
+            digits[i]++;
+            return digits;
+        }
+        digits[i] = 0;
+    }
+    return [1, ...digits];
+};
