897c9fb25a581359e0caf3fc5c9e5ff89ff4269d
ivan <ivan1016017@gmail.com>
2026-04-18 04:32:48 -0600

    adding updates

---
+++ tests/test_150_questions_round_22/test_135_coin_change_round_22.py
@@ -0,0 +1,8 @@
+import unittest
+from src.my_project.interviews.top_150_questions_round_22\
+.ex_135_coin_change import Solution
+
+
+class CoinChangeTestCase(unittest.TestCase):
+    def setUp(self):
+        self.solution = Solution()
