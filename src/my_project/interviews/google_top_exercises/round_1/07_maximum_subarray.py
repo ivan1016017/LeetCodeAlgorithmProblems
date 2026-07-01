1a7c6cdd5747c0d1b8e6509fc8d0a0a8969766de
ivan <ivan1016017@gmail.com>
2026-07-01 05:06:32 -0600

    adding updates

---
+++ src/my_project/interviews/google_top_exercises/round_1/07_maximum_subarray.py
@@ -0,0 +1,18 @@
+from typing import List 
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+
+        n = len(nums)
+        local_maximum = 0
+        global_maximum = -(10*10)
+
+        for i in range(n):
+
+            local_maximum = max(nums[i], nums[i] + local_maximum)
+
+            if local_maximum > global_maximum:
+                global_maximum = local_maximum
+
+        return global_maximum
+        
